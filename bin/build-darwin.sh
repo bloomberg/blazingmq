cc673994e437f46dcdfd37420b515351f54e6ab1
Patrick M. Niedzielski <patrick@pniedzielski.net>
2023-08-04 11:34:07 -0400

    Fix ShellCheck linter warnings on `build-darwin.sh`
    This patch fixes the warnings that our ShellCheck linter catches on
    the `build-darwin.sh` file.  Most of these changes are about quoting
    expanded variables, which in our case is generally good: for example,
    we want to correctly handle the case of a user’s build directory
    having a space in its full path.  In the case of `CMAKE_OPTIONS`, we
    do this by changing the string variable into an array of strings we
    can correctly quote, both when creating the array and when expanding
    it as arguments.
    
    Signed-off-by: Patrick M. Niedzielski <patrick@pniedzielski.net>

--- bin/build-darwin.sh
+++ bin/build-darwin.sh
@@ -15,17 +15,17 @@
 
 set -e
 set -u
-[ -z $BASH ] || shopt -s expand_aliases
+[ -z "$BASH" ] || shopt -s expand_aliases
 
-script_path="bin/$(basename $0)"
+script_path="bin/$(basename "$0")"
 
-if [ ! -f $script_path ] || [ $(realpath $0) != $(realpath $script_path) ]; then
+if [ ! -f "$script_path" ] || [ "$(realpath "$0")" != "$(realpath "$script_path")" ]; then
     echo 'This script must be run from the root of the BlazingMQ repository.'
     exit 1
 fi
 
 # :: Set some initial constants :::::::::::::::::::::::::::::::::::::::::::::::
-DIR_ROOT="${DIR_ROOT:-`pwd`}"
+DIR_ROOT="${DIR_ROOT:-$(pwd)}"
 
 DIR_THIRDPARTY="${DIR_ROOT}/thirdparty"
 mkdir -p "${DIR_THIRDPARTY}"
@@ -54,11 +54,11 @@ PATH="${DIR_THIRDPARTY}/bde-tools/bin:$PATH"
 
 if [ ! -e "${DIR_BUILD}/bde/.complete" ]; then
     pushd "${DIR_THIRDPARTY}/bde"
-    eval `bbs_build_env -u opt_64_cpp17 -b "${DIR_BUILD}/bde"`
+    eval "$(bbs_build_env -u opt_64_cpp17 -b "${DIR_BUILD}/bde")"
     bbs_build configure --prefix="${DIR_ROOT}"
     bbs_build build --prefix="${DIR_ROOT}"
     bbs_build --install_dir="/" --prefix="${DIR_ROOT}" install
-    eval `bbs_build_env unset`
+    eval "$(bbs_build_env unset)"
     popd
     touch "${DIR_BUILD}/bde/.complete"-DBDE_BUILD_TARGET_64=1
 fi
@@ -78,7 +78,7 @@ fi
 brew install flex bison google-benchmark zlib
 
 # Determine paths based on Intel vs Apple Silicon CPU
-if [ $(uname -p) == 'arm' ]; then
+if [ "$(uname -p)" == 'arm' ]; then
     BREW_PKG_CONFIG_PATH="/opt/homebrew/lib/pkgconfig:/opt/homebrew/opt/zlib/lib/pkgconfig"
     FLEX_ROOT="/opt/homebrew/opt/flex"
 else
@@ -88,30 +88,30 @@ fi
 
 
 # :: Build the BlazingMQ repo :::::::::::::::::::::::::::::::::::::::::::::::::
-CMAKE_OPTIONS="\
+CMAKE_OPTIONS=(\
     -DBDE_BUILD_TARGET_64=1 \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_INSTALL_LIBDIR=${DIR_ROOT}/lib \
-    -DCMAKE_INSTALL_PREFIX=${DIR_ROOT} \
-    -DCMAKE_MODULE_PATH=${DIR_THIRDPARTY}/bde-tools/cmake;${DIR_THIRDPARTY}/bde-tools/BdeBuildSystem \
-    -DCMAKE_PREFIX_PATH=${DIR_ROOT} \
-    -DCMAKE_TOOLCHAIN_FILE=${DIR_THIRDPARTY}/bde-tools/BdeBuildSystem/toolchains/darwin/gcc-default.cmake \
+    -DCMAKE_INSTALL_LIBDIR="${DIR_ROOT}/lib" \
+    -DCMAKE_INSTALL_PREFIX="${DIR_ROOT}" \
+    -DCMAKE_MODULE_PATH="${DIR_THIRDPARTY}/bde-tools/cmake;${DIR_THIRDPARTY}/bde-tools/BdeBuildSystem" \
+    -DCMAKE_PREFIX_PATH="${DIR_ROOT}" \
+    -DCMAKE_TOOLCHAIN_FILE="${DIR_THIRDPARTY}/bde-tools/BdeBuildSystem/toolchains/darwin/gcc-default.cmake" \
     -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-    -DFLEX_ROOT=${FLEX_ROOT}"
+    -DFLEX_ROOT="${FLEX_ROOT}")
 
 PKG_CONFIG_PATH="${DIR_ROOT}/lib/pkgconfig:${BREW_PKG_CONFIG_PATH}" \
-cmake -B "${DIR_BUILD}/blazingmq" -S "${DIR_ROOT}" ${CMAKE_OPTIONS}
+cmake -B "${DIR_BUILD}/blazingmq" -S "${DIR_ROOT}" "${CMAKE_OPTIONS[@]}"
 make -C "${DIR_BUILD}/blazingmq" -j 16
 
 echo broker is here: "${DIR_BUILD}/blazingmq/src/applications/bmqbrkr/bmqbrkr.tsk"
 echo to run the broker: "${DIR_BUILD}/blazingmq/src/applications/bmqbrkr/run"
 echo tool is here: "${DIR_BUILD}/blazingmq/src/applications/bmqtool/bmqtool.tsk"
 
-SELF_HOST_ADDRESS="127.0.0.1       `hostname`"
+SELF_HOST_ADDRESS="127.0.0.1       $(hostname)"
 if ! (grep -q "$SELF_HOST_ADDRESS" /etc/hosts > /dev/null);
 then
-    echo "Warning: self hostname `hostname` not found in /etc/hosts"
+    echo "Warning: self hostname $(hostname) not found in /etc/hosts"
     echo "It might be necessary to add it manually to launch bmqbrkr:"
     echo "sudo bash -c \"echo \\\"$SELF_HOST_ADDRESS\\\" >> /etc/hosts\""
 fi
