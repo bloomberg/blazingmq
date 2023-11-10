02ef69c9d0260663c36672e806b805003b56c35d
Jonathan Adotey <jon@astrolabe-243.dynamic.rpi.edu>
2023-11-10 17:43:53 -0500

    Added headers for z_bmqa_Session

---
+++ src/groups/z_bmq/z_bmqa/z_bmqa_message.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDED_Z_BMQA_MESSAGE
+#define INCLUDED_Z_BMQA_MESSAGE
+
+typedef struct z_bmqa_MessageConfirmationCookie z_bmqa_MessageConfirmationCookie;
+
+typedef struct z_bmqa_Message z_bmqa_Message;
+
+
+#endif
