853f4eb815446be76520f7238407622f90097848
simon <simon@Pradeeps-MBP.lan>
2023-11-17 19:51:29 -0500

    test

--- src/groups/z_bmq/z_bmqa/z_bmqa_session.cpp
+++ src/groups/z_bmq/z_bmqa/z_bmqa_session.cpp
@@ -33,4 +33,62 @@ int z_bmqa_Session__stop(z_bmqa_Session* session_obj) {
     session_ptr->stop();
 
     return 0;
+} 
+
+
+int z_bmqa_Session__finalizeStop(z_bmqa_Session* session_obj){
+    using namespace BloombergLP;
+    
+    bmqa::Session* session_ptr = reinterpret_cast<bmqa::Session*>(session_obj);
+    session_ptr->finalizeStop();
+    return 0;
+}
+
+
+int z_bmqa_Session__finalizeStop(z_bmqa_Session* session_obj){
+    using namespace BloombergLP;
+    
+    bmqa::Session* session_ptr = reinterpret_cast<bmqa::Session*>(session_obj);
+    session_ptr->finalizeStop();
+    return 0;
+}
+
+int z_bmqa_Session__loadMessageEventBuilder(z_bmqa_Session* session_obj, z_bmqa_MessageEventBuilder** builder){
+    using namespace BloombergLP;
+    
+    bmqa::Session* session_ptr = reinterpret_cast<bmqa::Session*>(session_obj);
+    bmqa::MessageEventBuilder* builder_ptr = reinterpret_cast<bmqa::MessageEventBuilder*>(builder);
+
+    session_ptr->loadMessageEventBuilder(builder_ptr);
+    return 0;
+}
+
+int z_bmqa_Session__loadConfirmEventBuilder(z_bmqa_Session* session_obj, z_bmqa_ConfirmEventBuilder** builder){
+    using namespace BloombergLP;
+    
+    bmqa::Session* session_ptr = reinterpret_cast<bmqa::Session*>(session_obj);
+    bmqa::ConfirmEventBuilder* builder_ptr = reinterpret_cast<bmqa::ConfirmEventBuilder*>(builder);
+
+    session_ptr->loadConfirmEventBuilder(builder_ptr);
+    return 0;
+}
+
+int z_bmqa_Session__loadMessageProperties(z_bmqa_Session* session_obj, z_bmqa_MessageProperties** buffer){
+    using namespace BloombergLP;
+    
+    bmqa::Session* session_ptr = reinterpret_cast<bmqa::Session*>(session_obj);
+    bmqa::MessageProperties* buffer_ptr = reinterpret_cast<bmqa::MessageProperties*>(buffer);
+
+    session_ptr->loadMessageProperties(buffer_ptr);
+    return 0;
+}
+
+int z_bmqa_Session__getQueueIdWithUri(z_bmqa_Session* session_obj, z_bmqa_QueueId** queueId, const z_bmqt_Uri* uri){
+    using namespace BloombergLP;
+    
+    bmqa::Session* session_ptr = reinterpret_cast<bmqa::Session*>(session_obj);
+    bmqa::Q* buffer_ptr = reinterpret_cast<bmqa::MessageProperties*>(uri);
+
+    session_ptr->getQueueId(buffer_ptr);
+    return 0;
 }
