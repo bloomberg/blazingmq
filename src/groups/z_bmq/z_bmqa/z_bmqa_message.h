9459753a64a646b58f2d73555f5d2d9f0482c67e
simon <simon@Pradeeps-MBP.lan>
2023-12-01 01:18:04 -0500

    producer bare minimum

--- src/groups/z_bmq/z_bmqa/z_bmqa_message.h
+++ src/groups/z_bmq/z_bmqa/z_bmqa_message.h
@@ -1,9 +1,11 @@
 #ifndef INCLUDED_Z_BMQA_MESSAGE
 #define INCLUDED_Z_BMQA_MESSAGE
 
-typedef struct z_bmqa_MessageConfirmationCookie z_bmqa_MessageConfirmationCookie;
-
 typedef struct z_bmqa_Message z_bmqa_Message;
+
+int z_bmqa_Message__createEmpty(z_bmqa_Message** message_obj);
+
+int z_bmqa_Message_setDataRef(z_bmqa_Message** message_obj, const char* data, size_t length);
 
 
 #endif
