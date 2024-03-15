fc006cfbb38d8049c4ad31c55cb7c2fb22f7acb2
Simon-Sandrew <sandrewsimon@gmail.com>
2024-03-15 16:08:13 -0400

    changes

--- src/groups/wrappers/z_bmq/z_bmqt/z_bmqt_subscription.h
+++ src/groups/wrappers/z_bmq/z_bmqt/z_bmqt_subscription.h
@@ -14,10 +14,28 @@ extern "C" {
 
 typedef struct z_bmqt_SubscriptionHandle z_bmqt_SubscriptionHandle;
 
+/**
+ * @brief Creates a new subscription handle object.
+ *
+ * This function creates a new z_bmqt_SubscriptionHandle object and assigns its pointer to the provided pointer.
+ * 
+ * @param subscriptionHandle_obj A pointer to a pointer to a z_bmqt_SubscriptionHandle object.
+ *                               Upon successful completion, this pointer will hold the newly created subscription handle object.
+ * @param cid A pointer to a constant z_bmqt_CorrelationId object used to initialize the subscription handle.
+ * @return Returns 0 upon successful creation.
+ */
 int z_bmqt_SubscriptionHandle__create(
     z_bmqt_SubscriptionHandle** subscriptionHandle_obj,
     const z_bmqt_CorrelationId* cid);
 
+/**
+ * @brief Retrieves the ID of the subscription handle.
+ *
+ * This function returns the ID of the specified subscription handle.
+ * 
+ * @param subscriptionHandle_obj A pointer to a constant z_bmqt_SubscriptionHandle object from which to retrieve the ID.
+ * @return Returns the ID of the subscription handle.
+ */
 unsigned int z_bmqt_SubscriptionHandle__id(
     const z_bmqt_SubscriptionHandle* subscriptionHandle_obj);
 
@@ -36,20 +54,66 @@ enum SubscriptionExpressionEnum {
     e_SUBSCRIPTIONEXPRESSION_ERROR
 };
 
+
+/**
+ * @brief Creates a new subscription expression object.
+ *
+ * This function creates a new z_bmqt_SubscriptionExpression object and assigns its pointer to the provided pointer.
+ * 
+ * @param subscriptionExpression_obj A pointer to a pointer to a z_bmqt_SubscriptionExpression object.
+ *                                    Upon successful completion, this pointer will hold the newly created subscription expression object.
+ * @return Returns 0 upon successful creation.
+ */
 int z_bmqt_SubscriptionExpression__create(
     z_bmqt_SubscriptionExpression** subscriptionExpression_obj);
 
+/**
+ * @brief Creates a new subscription expression object from a string.
+ *
+ * This function creates a new z_bmqt_SubscriptionExpression object from the provided string expression and version.
+ * 
+ * @param subscriptionExpression_obj A pointer to a pointer to a z_bmqt_SubscriptionExpression object.
+ *                                    Upon successful completion, this pointer will hold the newly created subscription expression object.
+ * @param expression A pointer to a constant character string representing the subscription expression.
+ * @param version The version of the subscription expression.
+ * @return Returns 0 upon successful creation, 1 if an unsupported version is provided.
+ */
 int z_bmqt_SubscriptionExpression__createFromString(
     z_bmqt_SubscriptionExpression** subscriptionExpression_obj,
     const char*                     expression,
     SubscriptionExpressionEnum      version);
 
+/**
+ * @brief Retrieves the text of the subscription expression.
+ *
+ * This function returns the text of the specified subscription expression.
+ * 
+ * @param subscriptionExpression_obj A pointer to a constant z_bmqt_SubscriptionExpression object from which to retrieve the text.
+ * @return Returns a pointer to a constant character string representing the text of the subscription expression.
+ *         The returned pointer may become invalid if the underlying subscription expression object is modified or destroyed.
+ */
 const char* z_bmqt_SubscriptionExpression__text(
     const z_bmqt_SubscriptionExpression* subscriptionExpression_obj);
 
+/**
+ * @brief Retrieves the version of the subscription expression.
+ *
+ * This function returns the version of the specified subscription expression.
+ * 
+ * @param subscriptionExpression_obj A pointer to a constant z_bmqt_SubscriptionExpression object from which to retrieve the version.
+ * @return Returns the version of the subscription expression.
+ */
 SubscriptionExpressionEnum z_bmqt_SubscriptionExpression__version(
     const z_bmqt_SubscriptionExpression* subscriptionExpression_obj);
 
+/**
+ * @brief Checks whether the subscription expression is valid.
+ *
+ * This function checks whether the specified subscription expression is valid.
+ * 
+ * @param subscriptionExpression_obj A pointer to a constant z_bmqt_SubscriptionExpression object to check for validity.
+ * @return Returns true if the subscription expression is valid, false otherwise.
+ */
 bool z_bmqt_SubscriptionExpression__isValid(
     const z_bmqt_SubscriptionExpression* subscriptionExpression_obj);
 
@@ -59,45 +123,158 @@ bool z_bmqt_SubscriptionExpression__isValid(
 
 typedef struct z_bmqt_Subscription z_bmqt_Subscription;
 
+/**
+ * @brief Creates a new subscription object.
+ *
+ * This function creates a new z_bmqt_Subscription object and assigns its pointer to the provided pointer.
+ * 
+ * @param subscription_obj A pointer to a pointer to a z_bmqt_Subscription object.
+ *                         Upon successful completion, this pointer will hold the newly created subscription object.
+ * @return Returns 0 upon successful creation.
+ */
+
 int z_bmqt_Subscription__create(z_bmqt_Subscription** subscription_obj);
 
+/**
+ * @brief Creates a copy of a subscription object.
+ *
+ * This function creates a copy of the specified z_bmqt_Subscription object and assigns its pointer to the provided pointer.
+ * 
+ * @param subscription_obj A pointer to a pointer to a z_bmqt_Subscription object.
+ *                         Upon successful completion, this pointer will hold the newly created copy of the subscription object.
+ * @param other A pointer to a constant z_bmqt_Subscription object to copy.
+ * @return Returns 0 upon successful creation.
+ */
 int z_bmqt_Subscription__createCopy(z_bmqt_Subscription** subscription_obj,
                                     const z_bmqt_Subscription* other);
 
+/**
+ * @brief Sets the maximum number of unconfirmed messages for a subscription.
+ *
+ * This function sets the maximum number of unconfirmed messages for the specified subscription.
+ * 
+ * @param subscription_obj A pointer to a z_bmqt_Subscription object for which to set the maximum number of unconfirmed messages.
+ * @param value The value representing the maximum number of unconfirmed messages to set.
+ * @return Returns 0 upon successful setting.
+ */
 int z_bmqt_Subscription__setMaxUnconfirmedMessages(
     z_bmqt_Subscription* subscription_obj,
     int                  value);
 
+/**
+ * @brief Sets the maximum number of unconfirmed bytes for a subscription.
+ *
+ * This function sets the maximum number of unconfirmed bytes for the specified subscription.
+ * 
+ * @param subscription_obj A pointer to a z_bmqt_Subscription object for which to set the maximum number of unconfirmed bytes.
+ * @param value The value representing the maximum number of unconfirmed bytes to set.
+ * @return Returns 0 upon successful setting.
+ */
 int z_bmqt_Subscription__setMaxUnconfirmedBytes(
     z_bmqt_Subscription* subscription_obj,
     int                  value);
 
+/**
+ * @brief Sets the consumer priority for a subscription.
+ *
+ * This function sets the consumer priority for the specified subscription.
+ * 
+ * @param subscription_obj A pointer to a z_bmqt_Subscription object for which to set the consumer priority.
+ * @param value The value representing the consumer priority to set.
+ * @return Returns 0 upon successful setting.
+ */
 int z_bmqt_Subscription__setConsumerPriority(
     z_bmqt_Subscription* subscription_obj,
     int                  value);
 
+/**
+ * @brief Sets the subscription expression for a subscription.
+ *
+ * This function sets the subscription expression for the specified subscription.
+ * 
+ * @param subscription_obj A pointer to a z_bmqt_Subscription object for which to set the subscription expression.
+ * @param value A pointer to a constant z_bmqt_SubscriptionExpression object representing the subscription expression to set.
+ * @return Returns 0 upon successful setting.
+ */
 int z_bmqt_Subscription__setExpression(
     z_bmqt_Subscription*                 subscription_obj,
     const z_bmqt_SubscriptionExpression* value);
 
+/**
+ * @brief Retrieves the maximum number of unconfirmed messages for a subscription.
+ *
+ * This function returns the maximum number of unconfirmed messages for the specified subscription.
+ * 
+ * @param subscription_obj A pointer to a constant z_bmqt_Subscription object from which to retrieve the maximum number of unconfirmed messages.
+ * @return Returns the maximum number of unconfirmed messages for the subscription.
+ */
 int z_bmqt_Subscription__maxUnconfirmedMessages(
     const z_bmqt_Subscription* subscription_obj);
 
+/**
+ * @brief Retrieves the maximum number of unconfirmed bytes for a subscription.
+ *
+ * This function returns the maximum number of unconfirmed bytes for the specified subscription.
+ * 
+ * @param subscription_obj A pointer to a constant z_bmqt_Subscription object from which to retrieve the maximum number of unconfirmed bytes.
+ * @return Returns the maximum number of unconfirmed bytes for the subscription.
+ */
 int z_bmqt_Subscription__maxUnconfirmedBytes(
     const z_bmqt_Subscription* subscription_obj);
 
+/**
+ * @brief Retrieves the consumer priority for a subscription.
+ *
+ * This function returns the consumer priority for the specified subscription.
+ * 
+ * @param subscription_obj A pointer to a constant z_bmqt_Subscription object from which to retrieve the consumer priority.
+ * @return Returns the consumer priority for the subscription.
+ */
 int z_bmqt_Subscription__consumerPriority(
     const z_bmqt_Subscription* subscription_obj);
 
+/**
+ * @brief Retrieves the subscription expression for a subscription.
+ *
+ * This function returns a pointer to the subscription expression for the specified subscription.
+ * 
+ * @param subscription_obj A pointer to a constant z_bmqt_Subscription object from which to retrieve the subscription expression.
+ * @return Returns a pointer to a constant z_bmqt_SubscriptionExpression object representing the subscription expression.
+ *         The returned pointer may become invalid if the underlying subscription object is modified or destroyed.
+ */
 const z_bmqt_SubscriptionExpression*
 z_bmqt_Subscription__expression(const z_bmqt_Subscription* subscription_obj);
 
+/**
+ * @brief Checks whether the subscription specifies a maximum number of unconfirmed messages.
+ *
+ * This function checks whether the specified subscription specifies a maximum number of unconfirmed messages.
+ * 
+ * @param subscription_obj A pointer to a constant z_bmqt_Subscription object to check.
+ * @return Returns true if the subscription specifies a maximum number of unconfirmed messages, false otherwise.
+ */
 bool z_bmqt_Subscription__hasMaxUnconfirmedMessages(
     const z_bmqt_Subscription* subscription_obj);
 
+/**
+ * @brief Checks whether the subscription specifies a maximum number of unconfirmed bytes.
+ *
+ * This function checks whether the specified subscription specifies a maximum number of unconfirmed bytes.
+ * 
+ * @param subscription_obj A pointer to a constant z_bmqt_Subscription object to check.
+ * @return Returns true if the subscription specifies a maximum number of unconfirmed bytes, false otherwise.
+ */
 bool z_bmqt_Subscription__hasMaxUnconfirmedBytes(
     const z_bmqt_Subscription* subscription_obj);
 
+/**
+ * @brief Checks whether the subscription specifies a consumer priority.
+ *
+ * This function checks whether the specified subscription specifies a consumer priority.
+ * 
+ * @param subscription_obj A pointer to a constant z_bmqt_Subscription object to check.
+ * @return Returns true if the subscription specifies a consumer priority, false otherwise.
+ */
 bool z_bmqt_Subscription__hasConsumerPriority(
     const z_bmqt_Subscription* subscription_obj);
 
--- src/groups/wrappers/z_bmq/z_bmqt/z_bmqt_uri.h
+++ src/groups/wrappers/z_bmq/z_bmqt/z_bmqt_uri.h
@@ -11,6 +11,16 @@ extern "C" {
 
 typedef struct z_bmqt_Uri z_bmqt_Uri;
 
+/**
+ * @brief Creates a copy of a URI object.
+ *
+ * This function creates a copy of the specified z_bmqt_Uri object and assigns its pointer to the provided pointer.
+ * 
+ * @param uri_obj A pointer to a pointer to a z_bmqt_Uri object.
+ *                Upon successful completion, this pointer will hold the newly created copy of the URI object.
+ * @param to_copy A pointer to a z_bmqt_Uri object to copy.
+ * @return Returns 0 upon successful creation.
+ */
 int z_bmqt_Uri_create(z_bmqt_Uri** uri_obj, z_bmqt_Uri* to_copy);
 
 // ================
@@ -19,23 +29,87 @@ int z_bmqt_Uri_create(z_bmqt_Uri** uri_obj, z_bmqt_Uri* to_copy);
 
 typedef struct z_bmqt_UriBuilder z_bmqt_UriBuilder;
 
+/**
+ * @brief Creates a new URI builder object.
+ *
+ * This function creates a new z_bmqt_UriBuilder object and assigns its pointer to the provided pointer.
+ * 
+ * @param uribuilder_obj A pointer to a pointer to a z_bmqt_UriBuilder object.
+ *                       Upon successful completion, this pointer will hold the newly created URI builder object.
+ * @return Returns 0 upon successful creation.
+ */
 int z_bmqt_UriBuilder__create(z_bmqt_UriBuilder** uribuilder_obj);
 
+/**
+ * @brief Creates a URI builder object from an existing URI.
+ *
+ * This function creates a new z_bmqt_UriBuilder object initialized with the specified URI and assigns its pointer to the provided pointer.
+ * 
+ * @param uribuilder_obj A pointer to a pointer to a z_bmqt_UriBuilder object.
+ *                       Upon successful completion, this pointer will hold the newly created URI builder object.
+ * @param uri_obj A pointer to a constant z_bmqt_Uri object from which to create the URI builder.
+ * @return Returns 0 upon successful creation.
+ */
 int z_bmqt_UriBuilder__createFromUri(z_bmqt_UriBuilder** uribuilder_obj,
                                      const z_bmqt_Uri*   uri_obj);
-
+/**
+ * @brief Sets the domain for a URI builder.
+ *
+ * This function sets the domain for the specified URI builder.
+ * 
+ * @param uribuilder_obj A pointer to a z_bmqt_UriBuilder object for which to set the domain.
+ * @param value A pointer to a constant character string representing the domain to set.
+ * @return Returns 0 upon successful setting.
+ */
 int z_bmqt_UriBuilder__setDomain(z_bmqt_UriBuilder* uribuilder_obj,
                                  const char*        value);
 
+/**
+ * @brief Sets the tier for a URI builder.
+ *
+ * This function sets the tier for the specified URI builder.
+ * 
+ * @param uribuilder_obj A pointer to a z_bmqt_UriBuilder object for which to set the tier.
+ * @param value A pointer to a constant character string representing the tier to set.
+ * @return Returns 0 upon successful setting.
+ */
 int z_bmqt_UriBuilder__setTier(z_bmqt_UriBuilder* uribuilder_obj,
                                const char*        value);
 
+/**
+ * @brief Sets the qualified domain for a URI builder.
+ *
+ * This function sets the qualified domain for the specified URI builder.
+ * 
+ * @param uribuilder_obj A pointer to a z_bmqt_UriBuilder object for which to set the qualified domain.
+ * @param value A pointer to a constant character string representing the qualified domain to set.
+ * @return Returns 0 upon successful setting.
+ */
 int z_bmqt_UriBuilder__setQualifiedDomain(z_bmqt_UriBuilder* uribuilder_obj,
                                           const char*        value);
 
+/**
+ * @brief Sets the queue name for a URI builder.
+ *
+ * This function sets the queue name for the specified URI builder.
+ * 
+ * @param uribuilder_obj A pointer to a z_bmqt_UriBuilder object for which to set the queue name.
+ * @param value A pointer to a constant character string representing the queue name to set.
+ * @return Returns 0 upon successful setting.
+ */
 int z_bmqt_UriBuilder__setQueue(z_bmqt_UriBuilder* uribuilder_obj,
                                 const char*        value);
 
+/**
+ * @brief Retrieves the URI built by a URI builder.
+ *
+ * This function retrieves the URI built by the specified URI builder.
+ * 
+ * @param uribuilder_obj A pointer to a z_bmqt_UriBuilder object to build the URI from.
+ * @param uri_obj A pointer to a pointer to a z_bmqt_Uri object.
+ *                Upon successful completion, this pointer will hold the newly created URI object.
+ * @return Returns 0 upon successful retrieval of the URI.
+ */
 int z_bmqt_UriBuilder_uri(z_bmqt_UriBuilder* uribuilder_obj,
                           z_bmqt_Uri**       uri_obj);
 
