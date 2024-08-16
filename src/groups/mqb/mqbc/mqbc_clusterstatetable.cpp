56bac08f22a8536df6e61cdd647a7365ca61e920
Evgeny Malygin <emalygin@bloomberg.net>
2024-08-16 07:17:46 +0300

    Fix[MQB]: compilation warnings
    Signed-off-by: Evgeny Malygin <emalygin@bloomberg.net>

--- src/groups/mqb/mqbc/mqbc_clusterstatetable.cpp
+++ src/groups/mqb/mqbc/mqbc_clusterstatetable.cpp
@@ -75,7 +75,7 @@ bool ClusterStateTableState::fromAscii(ClusterStateTableState::Enum* out,
     if (bdlb::String::areEqualCaseless(                                       \
             toAscii(ClusterStateTableState::e_##M),                           \
             str.data(),                                                       \
-            str.length())) {                                                  \
+            static_cast<int>(str.length()))) {                                \
         *out = ClusterStateTableState::e_##M;                                 \
         return true;                                                          \
     }
