0f5c2a4fe964a04ae173eef29ae6186004d365c9
Evgeny Malygin <emalygin@bloomberg.net>
2024-08-16 07:29:35 +0300

    Update mqbnet_elector.cpp
    Signed-off-by: Evgeny Malygin <emalygin@bloomberg.net>

--- src/groups/mqb/mqbnet/mqbnet_elector.cpp
+++ src/groups/mqb/mqbnet/mqbnet_elector.cpp
@@ -206,7 +206,7 @@ bool ElectorTimerEventType::fromAscii(ElectorTimerEventType::Enum* out,
 #define CHECKVALUE(M)                                                         \
     if (bdlb::String::areEqualCaseless(toAscii(ElectorTimerEventType::e_##M), \
                                        str.data(),                            \
-                                       str.length())) {                       \
+                                       static_cast<int>(str.length()))) {     \
         *out = ElectorTimerEventType::e_##M;                                  \
         return true;                                                          \
     }
@@ -266,7 +266,7 @@ bool ElectorState::fromAscii(ElectorState::Enum*      out,
 #define CHECKVALUE(M)                                                         \
     if (bdlb::String::areEqualCaseless(toAscii(ElectorState::e_##M),          \
                                        str.data(),                            \
-                                       str.length())) {                       \
+                                       static_cast<int>(str.length()))) {     \
         *out = ElectorState::e_##M;                                           \
         return true;                                                          \
     }
