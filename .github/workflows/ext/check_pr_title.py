796c41119947171a30e326ce8862e88f3e66faf7
Evgeny Malygin <emalygin@bloomberg.net>
2024-09-30 19:43:08 +0100

    Update check_pr_title.py
    Signed-off-by: Evgeny Malygin <emalygin@bloomberg.net>

--- .github/workflows/ext/check_pr_title.py
+++ .github/workflows/ext/check_pr_title.py
@@ -1,13 +1,34 @@
 import os
 
+
 def check_pr_title():
     title = os.environ.get("PR_TITLE")
     if title is None or len(title) == 0:
-        raise RuntimeError("This script expects a non-empty environment variable PR_TITLE set")
+        raise RuntimeError(
+            "This script expects a non-empty environment variable PR_TITLE set"
+        )
     title = title.lower()
-    valid_prefixes = ["fix", "feat", "perf", "ci", "build", "revert", "ut", "it", "docs", "refactor", "misc"]
+    valid_prefixes = [
+        "fix",
+        "feat",
+        "perf",
+        "ci",
+        "build",
+        "revert",
+        "ut",
+        "it",
+        "docs",
+        "refactor",
+        "misc",
+        "test",
+    ]
     if not any(title.startswith(prefix.lower()) for prefix in valid_prefixes):
-        raise RuntimeError("PR title \"{}\" doesn't start with a valid prefix, allowed prefixes: {}".format(title, " ".join(valid_prefixes)))
+        raise RuntimeError(
+            "PR title \"{}\" doesn't start with a valid prefix, allowed prefixes: {}".format(
+                title, " ".join(valid_prefixes)
+            )
+        )
+
 
 if __name__ == "__main__":
     check_pr_title()
