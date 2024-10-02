e3ca26c5cb891b842af66a6f7837b8501e421d9f
Dmitrii Petukhov <dpetukhov@bloomberg.net>
2024-10-03 02:15:17 +0300

    Add comment explaining not recording last confirm
    Signed-off-by: Dmitrii Petukhov <dpetukhov@bloomberg.net>

--- src/groups/mqb/mqbs/mqbs_filebackedstorage.cpp
+++ src/groups/mqb/mqbs/mqbs_filebackedstorage.cpp
@@ -390,6 +390,10 @@ FileBackedStorage::confirm(const bmqt::MessageGUID& msgGUID,
     BSLS_ASSERT_SAFE(!handles.empty());
 
     if (0 == --it->second.d_refCount) {
+        // Outstanding refCount for this message is zero now.
+        // In this case we intentionally skip recording the last CONFIRM
+        // due to optimization of jounral file usage
+
         return mqbi::StorageResult::e_ZERO_REFERENCES;  // RETURN
     }
 
