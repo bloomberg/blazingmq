230e7b592318d0ac848d76ee83650837b76471f4
Dmitry Petukhov <petukhov.dmitry@gmail.com>
2024-12-11 20:34:47 +0200

    Update src/groups/mqb/mqbstat/mqbstat_queuestats.h
    Co-authored-by: Evgeny Malygin <678098@protonmail.com>
    Signed-off-by: Dmitry Petukhov <petukhov.dmitry@gmail.com>

--- src/groups/mqb/mqbstat/mqbstat_queuestats.h
+++ src/groups/mqb/mqbstat/mqbstat_queuestats.h
@@ -555,6 +555,7 @@ QueueStatsDomain::onEvent<QueueStatsDomain::EventType::Enum::e_NACK>(
     BSLS_ANNOTATION_UNUSED bsls::Types::Int64 value)
 {
     BSLS_ASSERT_SAFE(d_statContext_mp && "initialize was not called");
+    // For NACK, we don't care about the bytes value
     d_statContext_mp->adjustValue(DomainQueueStats::e_STAT_NACK, 1);
 }
 
