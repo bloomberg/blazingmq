b44b25b677277eef4347e118511a9f2188f496e5
Evgeny Malygin <emalygin@bloomberg.net>
2025-02-21 11:12:46 +0000

    Update bmqex_executor.t.cpp
    Signed-off-by: Evgeny Malygin <emalygin@bloomberg.net>

--- src/groups/bmq/bmqex/bmqex_executor.t.cpp
+++ src/groups/bmq/bmqex/bmqex_executor.t.cpp
@@ -115,7 +115,7 @@ class StatExecutor {
     void post(bslmf::MovableRef<FUNCTION> f) const
     {
         ++d_statistics_p->d_postCallCount;
-        f();
+        bslmf::MovableRefUtil::access(f)();
     }
 
     /// Perform `++statistics()->d_dispatchCallCount` followed by `f()`.
@@ -123,7 +123,7 @@ class StatExecutor {
     void dispatch(bslmf::MovableRef<FUNCTION> f) const
     {
         ++d_statistics_p->d_dispatchCallCount;
-        f();
+        bslmf::MovableRefUtil::access(f)();
     }
 
   public:
