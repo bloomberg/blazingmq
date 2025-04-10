596603d91aa2a25b2b04f9eaeadec6c8799888ab
Anton Pryakhin <apryakhin1@bloomberg.net>
2025-04-10 18:22:38 +0300

    Address review pt1
    Signed-off-by: Anton Pryakhin <apryakhin1@bloomberg.net>

--- src/integration-tests/test_connection_loss.py
+++ src/integration-tests/test_connection_loss.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Bloomberg Finance L.P.
+# Copyright 2024 Bloomberg Finance L.P.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -36,9 +36,9 @@ from blazingmq.dev.it.process.proc import Process
 pytestmark = order(2)
 
 
-def test_broker_client(
-    cluster: Cluster,  # pylint: disable=redefined-outer-name
-) -> None:
+@tweak.broker.app_config.network_interfaces.heartbeats.client(2)
+@tweak.broker.app_config.network_interfaces.tcp_interface.heartbeat_interval_ms(100)
+def test_broker_client(cluster: Cluster) -> None:
     """
     Test: connection loss between a broker and a client.
     - Start a broker and save the port it is listening.
@@ -51,46 +51,35 @@ def test_broker_client(
     - The connection is restored after tproxy restart.
     """
 
-    proxies = cluster.proxy_cycle()
-    proxy = next(proxies)
-    broker_host = proxy.config.host
-    broker_port = proxy.config.port
+    broker = next(cluster.proxy_cycle())
 
     # Start tproxy between broker's and client's ports
-    tproxy = Process("tproxy", ["tproxy", "-r", f"{broker_host}:{broker_port}"])
-    try:
-        tproxy.start()
-        tproxy_port = tproxy.capture(r"Listening on (.+):(\d+)", timeout=2).group(2)
+    tproxy_port, tproxy = cluster.start_tproxy(broker.config)
 
-        # Start a client
-        client: Client = proxy.create_client(f"client@{proxy.name}", port=tproxy_port)
-        client.start_session()
-        assert client.capture(r"CONNECTED", 5)
+    # Start a client
+    client: Client = broker.create_client(f"client@{broker.name}", port=tproxy_port)
+    client.start_session()
+    assert client.capture(r"CONNECTED", 5)
 
-        # Kill tproxy to break the connection between broker and client
-        tproxy.kill()
-        assert client.wait_connection_lost(5)
+    # Kill tproxy to break the connection between broker and client
+    tproxy.kill()
+    assert client.wait_connection_lost(5)
 
-        # Start tproxy to restore the connection between broker and client
-        tproxy = Process(
-            "tproxy",
-            ["tproxy", "-r", f"{broker_host}:{broker_port}", "-p", f"{tproxy_port}"],
-        )
-        tproxy.start()
-        assert client.capture(r"RECONNECTED", 5)
+    # Start tproxy to restore the connection between broker and client
+    cluster.start_tproxy(broker.config, port=tproxy_port)
+    assert client.capture(r"RECONNECTED", 5)
 
-        client.exit_gracefully()
-        client.wait(5)
-    finally:
-        tproxy.kill()
+    client.exit_gracefully()
+    client.wait(5)
 
 
 @start_cluster(False)
 @tweak.cluster.cluster_attributes.is_cslmode_enabled(False)
 @tweak.cluster.cluster_attributes.is_fsmworkflow(False)
-def test_force_leader_primary_divergence(  # pylint: disable=too-many-locals
-    multi_node: Cluster,  # pylint: disable=redefined-outer-name
-) -> None:
+@tweak.cluster.elector.quorum(4)
+@tweak.broker.app_config.network_interfaces.heartbeats.cluster_peer(3)
+@tweak.broker.app_config.network_interfaces.tcp_interface.heartbeat_interval_ms(100)
+def test_force_leader_primary_divergence(multi_node: Cluster) -> None:
     """
     Test: connection loss between cluster nodes.
     - Run three instances of tproxy redirecting to endpoints of nodes "east2", "west1" and "west2".
@@ -119,84 +108,63 @@ def test_force_leader_primary_divergence(  # pylint: disable=too-many-locals
      partitions is "east1"
     """
 
-    cluster = multi_node  # pylint: disable=redefined-outer-name
+    cluster = multi_node
     tproxies: Dict[str, Process] = {}
 
-    try:
-        for name in cluster.config.nodes:
-            with open(
-                cluster.work_dir.joinpath(
-                    cluster.config.nodes[name].config_dir, "clusters.json"
-                ),
-                "r+",
-                encoding="utf-8",
-            ) as f:
-                data = json.load(f)
-                if name == "east1":
-                    # Modify cluster config for node "east1"
-                    for node in data["myClusters"][0]["nodes"]:
-                        if node["name"] != name:
-                            # For all the nodes except "east1" start a tproxy connected to the
-                            # node's endpoint. Change the endpoint in the config to the port of
-                            # just started tproxy. So "east1" will connect to tproxies instead of
-                            # the nodes.
-                            broker_config = cluster.config.nodes[node["name"]]
-                            tproxy = Process(
-                                "tproxy",
-                                [
-                                    "tproxy",
-                                    "-r",
-                                    f"{broker_config.host}:{broker_config.port}",
-                                ],
-                            )
-                            tproxy.start()
-                            tproxy_port = tproxy.capture(
-                                r"Listening on (.+):(\d+)", timeout=2
-                            ).group(2)
-                            tproxies[node["name"]] = tproxy
-                            node["transport"]["tcp"]["endpoint"] = node["transport"][
-                                "tcp"
-                            ]["endpoint"].replace(str(broker_config.port), tproxy_port)
-                else:
-                    data["myClusters"][0]["elector"]["quorum"] = 4
-                f.seek(0)
-                json.dump(data, f, indent=4)
-                f.truncate()
+    # Modify cluster config for node "east1"
+    with open(
+        cluster.work_dir.joinpath(
+            cluster.config.nodes["east1"].config_dir, "clusters.json"
+        ),
+        "r+",
+        encoding="utf-8",
+    ) as f:
+        data = json.load(f)
+        data["myClusters"][0]["elector"]["quorum"] = 0
+        for node in data["myClusters"][0]["nodes"]:
+            if node["name"] != "east1":
+                # For all the nodes except "east1" start a tproxy connected to the
+                # node's endpoint. Change the endpoint in the config to the port of
+                # just started tproxy. So "east1" will connect to tproxies instead of
+                # the nodes.
+                broker_config = cluster.config.nodes[node["name"]]
+                tproxy_port, tproxy = cluster.start_tproxy(broker_config)
+                tproxies[tproxy.name] = tproxy
+                node["transport"]["tcp"]["endpoint"] = node["transport"]["tcp"][
+                    "endpoint"
+                ].replace(str(broker_config.port), tproxy_port)
+        f.seek(0)
+        json.dump(data, f, indent=4)
+        f.truncate()
 
-        # Start east1, west1, and west2
-        cluster.start_node("east1")
-        cluster.start_node("west1")
-        cluster.start_node("west2")
+    # Start east1, west1, and west2
+    cluster.start_node("east1")
+    cluster.start_node("west1")
+    cluster.start_node("west2")
 
-        # Wait until "east1" becomes leader. It is the only possible leader because only it has
-        # quorum = 3
-        leader = cluster.wait_leader()
-        assert leader.name == "east1"
+    # Wait until "east1" becomes leader. It is the only possible leader because only it has
+    # quorum = 3
+    leader = cluster.wait_leader()
+    assert leader.name == "east1"
 
-        # Start "east2" and kill two tproxies disconnecting "east2" and "west1" from "east1".
-        cluster.start_node("east2")
-        tproxies["east2"].kill()
-        tproxies["west1"].kill()
+    # Start "east2" and kill two tproxies disconnecting "east2" and "west1" from "east1".
+    cluster.start_node("east2")
+    tproxies["tproxy_east2"].kill()
+    tproxies["tproxy_west1"].kill()
 
-        # Leader must become "west2" as it is the only node connected to all other nodes
-        leader = cluster.wait_leader()
-        assert leader.name == "west2"
+    # Leader must become "west2" as it is the only node connected to all other nodes
+    leader = cluster.wait_leader()
+    assert leader.name == "west2"
 
-        # Request partitions summary with admin command. Check that primary for all partitions is
-        # "east1"
-        admin = AdminClient()
-        admin.connect(leader.config.host, leader.config.port)
-        res = admin.send_admin(
-            f"CLUSTERS CLUSTER {cluster.config.name} STORAGE SUMMARY"
-        )
-        primaries: [str] = []
-        for line in res.splitlines():
-            m = re.search(r"Primary Node.*\[(.+), \d+\]", line)
-            if m:
-                assert m.group(1) == "east1"
-                primaries.append(m.group(1))
-        assert len(primaries) == 4
-    finally:
-        # Kill the tproxies
-        for tp in tproxies.values():
-            tp.kill()
+    # Request partitions summary with admin command. Check that primary for all partitions is
+    # "east1"
+    admin = AdminClient()
+    admin.connect(leader.config.host, leader.config.port)
+    res = admin.send_admin(f"CLUSTERS CLUSTER {cluster.config.name} STORAGE SUMMARY")
+    primaries: [str] = []
+    for line in res.splitlines():
+        m = re.search(r"Primary Node.*\[(.+), \d+\]", line)
+        if m:
+            assert m.group(1) == "east1"
+            primaries.append(m.group(1))
+    assert len(primaries) == 4
