338d9a9233ac8f7aad0dec322f5d40b466ee0ac8
Emelia Lei <wlei29@bloomberg.net>
2025-04-23 14:48:58 -0400

    modify authenticator
    Signed-off-by: Emelia Lei <wlei29@bloomberg.net>

--- src/groups/mqb/mqba/mqba_authenticator.cpp
+++ src/groups/mqb/mqba/mqba_authenticator.cpp
@@ -47,7 +47,48 @@ int Authenticator::onAuthenticationRequest(
     bsl::ostream&                  errorDescription,
     const AuthenticationContextSp& context)
 {
-    return 0;
+    // PRECONDITIONS
+    BSLS_ASSERT_SAFE(
+        context->d_authenticationMessage.isAuthenticateRequestValue());
+    BSLS_ASSERT_SAFE(context->d_initialConnectionContext_p->isIncoming() ||
+                     context->d_isReversed);
+
+    bmqp_ctrlmsg::AuthenticateRequest& authenticateRequest =
+        context->d_authenticationMessage.authenticateRequest();
+
+    BALL_LOG_DEBUG
+        << "Received authentication message from '"
+        << context->d_initialConnectionContext_p->channel()->peerUri()
+        << "': " << authenticateRequest;
+
+    bmqp_ctrlmsg::AuthenticationMessage authenticationResponse;
+    bmqp_ctrlmsg::AuthenticateResponse& response =
+        authenticationResponse.makeAuthenticateResponse();
+
+    // TODO: authenticate
+    if (authenticateRequest.mechanism() == "") {
+        BALL_LOG_ERROR << "Error on authentication";
+
+        bmqu::MemOutStream os;
+        os << "Mechanism is unspecified";
+        response.status().category() =
+            bmqp_ctrlmsg::StatusCategory::E_NOT_SUPPORTED;
+        response.status().message() = os.str();
+        response.status().code()    = -1;
+    }
+    else {
+        response.status().category() = bmqp_ctrlmsg::StatusCategory::E_SUCCESS;
+        response.status().code()     = 0;
+        response.lifetimeMs()        = 10 * 60 * 1000;
+    }
+
+    BALL_LOG_INFO << "send authn response " << authenticationResponse;
+
+    int rc = sendAuthenticationMessage(errorDescription,
+                                       authenticationResponse,
+                                       context);
+
+    return rc;
 }
 
 int Authenticator::onAuthenticationResponse(
@@ -62,7 +103,41 @@ int Authenticator::sendAuthenticationMessage(
     const bmqp_ctrlmsg::AuthenticationMessage& message,
     const AuthenticationContextSp&             context)
 {
-    return 0;
+    enum RcEnum {
+        // Value for the various RC error categories
+        rc_SUCCESS       = 0,
+        rc_BUILD_FAILURE = -1,
+        rc_WRITE_FAILURE = -2
+    };
+
+    bmqp::EncodingType::Enum encodingType = bmqp::EncodingType::e_BER;
+
+    // TODO: why do we create a local allocator?
+    bdlma::LocalSequentialAllocator<2048> localAllocator(d_allocator_p);
+
+    bmqp::SchemaEventBuilder builder(d_blobSpPool_p,
+                                     encodingType,
+                                     &localAllocator);
+
+    int rc = builder.setMessage(message, bmqp::EventType::e_AUTHENTICATION);
+    if (rc != 0) {
+        errorDescription << "Failed building AuthenticationMessage "
+                         << "[rc: " << rc << ", message: " << message << "]";
+        return rc_BUILD_FAILURE;  // RETURN
+    }
+
+    // Send response event
+    bmqio::Status status;
+    context->d_initialConnectionContext_p->channel()->write(&status,
+                                                            *builder.blob());
+    if (!status) {
+        errorDescription << "Failed sending AuthenticationMessage "
+                         << "[status: " << status << ", message: " << message
+                         << "]";
+        return rc_WRITE_FAILURE;  // RETURN
+    }
+
+    return rc_SUCCESS;
 }
 
 void Authenticator::initiateOutboundAuthentication(
@@ -71,8 +146,10 @@ void Authenticator::initiateOutboundAuthentication(
 }
 
 // CREATORS
-Authenticator::Authenticator(bslma::Allocator* allocator)
+Authenticator::Authenticator(BlobSpPool*       blobSpPool,
+                             bslma::Allocator* allocator)
 : d_allocator_p(allocator)
+, d_blobSpPool_p(blobSpPool)
 , d_clusterCatalog_p(0)
 {
     // NOTHING
@@ -82,6 +159,53 @@ Authenticator::Authenticator(bslma::Allocator* allocator)
 Authenticator::~Authenticator()
 {
     // NOTHING: (required because of inheritance)
+}
+
+int Authenticator::handleAuthenticationOnMsgType(
+    const AuthenticationContextSp& context)
+{
+    enum RcEnum {
+        // Value for the various RC error categories
+        rc_SUCCESS = 0,
+        rc_ERROR   = -1,
+    };
+
+    bmqu::MemOutStream errStream;
+    int                rc = rc_SUCCESS;
+
+    switch (context->d_authenticationMessage.selectionId()) {
+    case bmqp_ctrlmsg::AuthenticationMessage::
+        SELECTION_ID_AUTHENTICATE_REQUEST: {
+        BALL_LOG_INFO << "Received authn request: "
+                      << context->d_authenticationMessage;
+        rc = onAuthenticationRequest(errStream, context);
+    } break;  // BREAK
+    case bmqp_ctrlmsg::AuthenticationMessage::
+        SELECTION_ID_AUTHENTICATE_RESPONSE: {
+        BALL_LOG_INFO << "Received authn response: "
+                      << context->d_authenticationMessage;
+    } break;  // BREAK
+    default: {
+        errStream << "Invalid authentication message received (unknown type): "
+                  << context->d_authenticationMessage;
+        bsl::string error(errStream.str().data(), errStream.str().length());
+        context->d_initialConnectionContext_p->initialConnectionCompleteCb()(
+            rc_ERROR,
+            error,
+            bsl::shared_ptr<mqbnet::Session>());
+        return rc_ERROR;  // RETURN
+    }
+    }
+
+    if (rc != rc_SUCCESS) {
+        bsl::string error(errStream.str().data(), errStream.str().length());
+        context->d_initialConnectionContext_p->initialConnectionCompleteCb()(
+            rc_ERROR,
+            error,
+            bsl::shared_ptr<mqbnet::Session>());
+    }
+
+    return rc;
 }
 
 int Authenticator::authenticationOutboundOrReverse(
