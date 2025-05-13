d05f28f5806ee5469c96683affe58db3345edad1
Emelia Lei <wlei29@bloomberg.net>
2025-05-13 14:42:05 -0400

    create pass plugin
    Signed-off-by: Emelia Lei <wlei29@bloomberg.net>

---
+++ src/plugins/bmqauthnpass/bmqauthnpass_passauthenticator.cpp
@@ -0,0 +1,152 @@
+// Copyright 2016-2025 Bloomberg Finance L.P.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <bmqauthnpass_passauthenticator.h>
+
+// BMQAUTHNPASS
+#include <bmqauthnpass_version.h>
+
+// MQB
+#include <mqbplug_authenticator.h>
+
+// BMQ
+
+// BDE
+#include <bsl_iostream.h>
+#include <bsl_memory.h>
+#include <bsl_optional.h>
+#include <bsl_string.h>
+#include <bslma_allocator.h>
+#include <bslma_managedptr.h>
+#include <bsls_assert.h>
+
+namespace BloombergLP {
+namespace bmqauthnpass {
+
+// ------------------------------
+// class PassAuthenticationResult
+// ------------------------------
+
+PassAuthenticationResult::PassAuthenticationResult(
+    const bslstl::StringRef& principal,
+    bsls::Types::Int64       lifetimeMs,
+    bslma::Allocator*        allocator)
+: d_principal(principal)
+, d_lifetimeMs(lifetimeMs)
+, d_allocator_p(allocator)
+{
+}
+
+PassAuthenticationResult::~PassAuthenticationResult()
+{
+}
+
+const bsl::string& PassAuthenticationResult::principal() const
+{
+    return d_principal;
+}
+
+const bsl::optional<bsls::Types::Int64>&
+PassAuthenticationResult::lifetimeMs() const
+{
+    return d_lifetimeMs;
+}
+
+// -----------------------
+// class PassAuthenticator
+// -----------------------
+
+PassAuthenticator::PassAuthenticator(bslma::Allocator* allocator)
+: d_authenticatorConfig_p(
+      mqbplug::AuthenticatorUtil::findAuthenticatorConfig(name()))
+, d_isStarted(false)
+, d_allocator_p(allocator)
+{
+}
+
+PassAuthenticator::~PassAuthenticator()
+{
+    stop();
+}
+
+inline bslstl::StringRef PassAuthenticator::name() const
+{
+    return "PassAuthenticator";
+}
+
+inline bslstl::StringRef PassAuthenticator::mechanism() const
+{
+    return "Basic";
+}
+
+int PassAuthenticator::authenticate(
+    bsl::ostream&                                   errorDescription,
+    bsl::shared_ptr<mqbplug::AuthenticationResult>* result,
+    const mqbplug::AuthenticationData&              input) const
+{
+    *result = bsl::allocate_shared<PassAuthenticationResult>(d_allocator_p,
+                                                             "",
+                                                             600 * 1000);
+    return 0;
+}
+
+int PassAuthenticator::start(bsl::ostream& errorDescription)
+{
+    if (!d_authenticatorConfig_p) {
+        errorDescription << "Could not find config for PassAuthenticator '"
+                         << name() << "' with mechanism '" << mechanism()
+                         << "'";
+        return -1;  // RETURN
+    }
+
+    if (d_isStarted) {
+        return 0;  // RETURN
+    }
+
+    d_isStarted = true;
+
+    return 0;
+}
+
+void PassAuthenticator::stop()
+{
+    d_isStarted = false;
+}
+
+// ------------------------------------
+// class PassAuthenticatorPluginFactory
+// ------------------------------------
+
+PassAuthenticatorPluginFactory::PassAuthenticatorPluginFactory()
+{
+    // NOTHING
+}
+
+PassAuthenticatorPluginFactory::~PassAuthenticatorPluginFactory()
+{
+    // NOTHING
+}
+
+bslma::ManagedPtr<mqbplug::Authenticator>
+PassAuthenticatorPluginFactory::create(bslma::Allocator* allocator)
+{
+    bslma::ManagedPtr<mqbplug::Authenticator> result(
+        new (*allocator) PassAuthenticator(allocator),
+        allocator);
+    return result;
+}
+
+}  // close package namespace
+}  // close enterprise namespace
