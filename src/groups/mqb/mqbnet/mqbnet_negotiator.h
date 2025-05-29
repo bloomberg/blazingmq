7c0c099662337346a558f7eab746bad07900fe48
Emelia Lei <69409730+emelialei88@users.noreply.github.com>
2025-05-29 10:15:55 -0400

    Feat[MQB]: add authentication with basic logic (#746)
    Signed-off-by: Emelia Lei <wlei29@bloomberg.net>

--- src/groups/mqb/mqbnet/mqbnet_negotiator.h
+++ src/groups/mqb/mqbnet/mqbnet_negotiator.h
@@ -20,24 +20,14 @@
 //@PURPOSE: Provide a protocol for a session negotiator.
 //
 //@CLASSES:
-//  mqbnet::Negotiator:        protocol for a session negotiator
+//  mqbnet::Negotiator: protocol for a session negotiator
 //
 //@SEE_ALSO:
 //  mqbnet::Session: protocol of the negotiated sessions
 //
 //@DESCRIPTION: 'mqbnet::Negotiator' is a protocol for a session negotiator
 // that uses a provided established channel to negotiate and create an
-// 'mqbnet::Session' object.  'mqbnet::InitialConnectionContext' is a
-// value-semantic type holding the context associated with a session being
-// negotiated.  It allows bi-directional generic communication between the
-// application layer and the transport layer: for example, a user data
-// information can be passed in at application layer, kept and carried over in
-// the transport layer and retrieved in the negotiator concrete implementation.
-// Similarly, a 'cookie' can be passed in from application layer, to the result
-// callback notification in the transport layer (usefull for 'listen-like'
-// established connection where the entry point doesn't allow to bind specific
-// user data, which then can be retrieved at application layer during
-// negotiation).
+// 'mqbnet::Session' object.
 //
 // Note that the 'InitialConnectionContext' passed in to the
 // 'InitialConnectionHandler::handleInitialConnection()' method can be modified
@@ -72,7 +62,7 @@ class Negotiator {
 
     // MANIPULATORS
 
-    /// Create a `session` based on the type of initial connection message in
+    /// Create a `session` based on the type of NegotiationMessage in
     /// the specified `context`.  Set `isContinueRead` to true if we want to
     /// continue reading instead of creating a session just yet.
     /// Return 0 on success, or a non-zero error code and populate the
