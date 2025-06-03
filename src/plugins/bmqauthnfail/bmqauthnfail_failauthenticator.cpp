d2f56f58f4113a83b90b8f131684a7c450b49607
Emelia Lei <wlei29@bloomberg.net>
2025-06-03 16:52:09 -0400

    small fix on authn plug
    Signed-off-by: Emelia Lei <wlei29@bloomberg.net>

--- src/plugins/bmqauthnfail/bmqauthnfail_failauthenticator.cpp
+++ src/plugins/bmqauthnfail/bmqauthnfail_failauthenticator.cpp
@@ -94,10 +94,18 @@ bslstl::StringRef FailAuthenticator::mechanism() const
 }
 
 int FailAuthenticator::authenticate(
-    BSLA_UNUSED bsl::ostream&                       errorDescription,
+    bsl::ostream&                                   errorDescription,
     bsl::shared_ptr<mqbplug::AuthenticationResult>* result,
     BSLA_UNUSED const mqbplug::AuthenticationData& input) const
 {
+    BALL_LOG_INFO << "FailAuthenticator: "
+                  << "authentication failed for mechanism '" << mechanism()
+                  << "' unconditionally.";
+
+    errorDescription << "FailAuthenticator: "
+                     << "authentication failed for mechanism '" << mechanism()
+                     << "' unconditionally.";
+
     *result = bsl::allocate_shared<FailAuthenticationResult>(d_allocator_p,
                                                              "",
                                                              600 * 1000);
