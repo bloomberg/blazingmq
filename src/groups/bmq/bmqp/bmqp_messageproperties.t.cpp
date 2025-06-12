d9416373ba643c34a76b4fa5ccf5540517a6b11e
Patrick M. Niedzielski <patrick@pniedzielski.net>
2025-06-12 17:48:51 -0400

    Fix: Disable self-assignment warning in test
    This patch turns off the following warning emitted by Clang in `bmqp`:
    
        src/groups/bmq/bmqp/bmqp_messageproperties.t.cpp:1213:23: warning: explicitly assigning value of variable of type 'bmqp::MessageProperties' to itself [-Wself-assign-overloaded]
         1213 |     obj2              = obj2;
              |     ~~~~              ^ ~~~~
        1 warning generated.
    
    This self-assignment is intentional; the test is checking that
    self-assignment is properly implemented.
    
    Signed-off-by: Patrick M. Niedzielski <patrick@pniedzielski.net>

--- src/groups/bmq/bmqp/bmqp_messageproperties.t.cpp
+++ src/groups/bmq/bmqp/bmqp_messageproperties.t.cpp
@@ -1209,9 +1209,12 @@ static void test9_copyAssignTest()
     verify(&pmap2, obj2);
 
     // Check self assignment
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wself-assign-overloaded"
     PropertyMap pmap3 = pmap;
     obj2              = obj2;
     verify(&pmap3, obj2);
+#pragma clang diagnostic pop
 }
 
 static void test10_empty()
