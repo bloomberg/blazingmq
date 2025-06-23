7b198d1c20cc363dfeaa91f4ccd5e377050415bb
Emelia Lei <wlei29@bloomberg.net>
2025-06-23 17:16:29 -0400

    add bmqauthnbasic plugin and more tests for reauthn
    Signed-off-by: Emelia Lei <wlei29@bloomberg.net>

---
+++ src/plugins/bmqauthnbasic/bmqauthnbasic_version.h
@@ -0,0 +1,108 @@
+// Copyright 2016-2025 Bloomberg Finance L.P.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_AUTHNBASIC_VERSION
+#define INCLUDED_AUTHNBASIC_VERSION
+
+//@PURPOSE: Provide source control management (versioning) information.
+//
+//@CLASSES:
+//  bmqauthnbasic::Version: namespace for 'bmqauthnbasic' SCM versioning
+//  information
+//
+//@DESCRIPTION: This component provides source control management (versioning)
+// information for the 'bmqauthnbasic' plugin.  In particular, this component
+// embeds RCS-style and SCCS-style version strings in binary executable files
+// that use one or more components from the 'bmqauthnbasic' plugin.  This
+// version information may be extracted from binary files using common UNIX
+// utilities (e.g., 'ident' and 'what').  In addition, the 'version' 'static'
+// member function in the 'bmqauthnbasic::Version' struct can be used to query
+// version information for the 'bmqauthnbasic' plugin at runtime.  The
+// following USAGE examples illustrate these two basic capabilities.
+//
+// Note that unless the 'version' method will be called, it is not necessary to
+// "#include" this component header file to get 'bmqauthnbasic' version
+// information embedded in an executable.  It is only necessary to use one or
+// more 'bmqauthnbasic' components (and, hence, link in the 'bmqauthnbasic'
+// library).
+
+// AUTHNBASIC
+#include <bmqauthnbasic_versiontag.h>
+
+// BDE
+#include <bsls_linkcoercion.h>
+
+namespace BloombergLP {
+namespace bmqauthnbasic {
+
+struct Version {
+    // PUBLIC CLASS DATA
+    static const char* s_ident;
+    static const char* s_what;
+
+#define AUTHNBASIC_CONCAT2(a, b, c, d, e) a##b##c##d##e
+#define AUTHNBASIC_CONCAT(a, b, c, d, e) AUTHNBASIC_CONCAT2(a, b, c, d, e)
+
+// 'AUTHNBASIC_S_VERSION' is a symbol whose name warns users of version
+// mismatch linking errors.  Note that the exact string "compiled_this_object"
+// must be present in this version coercion symbol.  Tools may look for this
+// pattern to warn users of mismatches.
+#define AUTHNBASIC_S_VERSION                                                  \
+    AUTHNBASIC_CONCAT(d_version_AUTHNBASIC_,                                  \
+                      AUTHNBASIC_VERSION_MAJOR,                               \
+                      _,                                                      \
+                      AUTHNBASIC_VERSION_MINOR,                               \
+                      _compiled_this_object)
+
+    static const char* AUTHNBASIC_S_VERSION;
+
+    static const char* s_dependencies;
+    static const char* s_buildInfo;
+    static const char* s_timestamp;
+    static const char* s_sourceControlInfo;
+
+    // CLASS METHODS
+    static const char* version();
+    // Return the formatted string corresponding to the version. Format is
+    // BLP_LIB_AUTHNBASIC_<major>.<minor>.<patch>
+
+    static int versionAsInt();
+    // Return the int corresponding to the version, using the following
+    // formula: '(major) * 10000 + (minor) * 100 + (patch)'
+};
+
+// ============================================================================
+//                             INLINE DEFINITIONS
+// ============================================================================
+
+// ---------------------------
+// class bmqauthnbasic::Version
+// ---------------------------
+
+inline const char* Version::version()
+{
+    return AUTHNBASIC_S_VERSION;
+}
+
+inline int Version::versionAsInt()
+{
+    return AUTHNBASIC_EXT_VERSION;
+}
+
+}  // close package namespace
+
+}  // close enterprise namespace
+
+#endif
---
+++ src/plugins/bmqauthnbasic/bmqauthnbasic_versiontag.h
@@ -0,0 +1,94 @@
+// Copyright 2016-2025 Bloomberg Finance L.P.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_AUTHNBASIC_VERSIONTAG
+#define INCLUDED_AUTHNBASIC_VERSIONTAG
+
+//@PURPOSE: Provide versioning information for the 'bmqauthnbasic' plugin.
+//
+//@SEE_ALSO: bmqauthnbasic::Version
+//
+//@DESCRIPTION: This component provides versioning information for the
+// 'bmqauthnbasic' plugin.  The 'AUTHNBASIC_VERSION' macro that is supplied can
+// be used for conditional-compilation based on 'bmqauthnbasic' version
+// information. The following usage example illustrates this basic capability.
+//
+/// Usage
+///-----
+// At compile time, the version of AUTHNBASIC can be used to select an older or
+// newer way to accomplish a task, to enable new functionality, or to
+// accommodate an interface change.  For example, if the name of a function
+// changes (a rare occurrence, but potentially disruptive when it does happen),
+// the impact on affected code can be minimized by conditionally calling the
+// function by its old or new name using conditional compilation.  In the
+// following, the '#if' preprocessor directive compares 'AUTHNBASIC_VERSION'
+// (i.e., the latest AUTHNBASIC version, excluding the patch version) to a
+// specified major and minor version composed using the 'BDE_MAKE_VERSION'
+// macro:
+//..
+//  #if AUTHNBASIC_VERSION > BDE_MAKE_VERSION(1, 3)
+//      // Call 'newFunction' for AUTHNBASIC versions later than 1.3.
+//      int result = newFunction();
+//  #else
+//      // Call 'oldFunction' for AUTHNBASIC version 1.3 or earlier.
+//      int result = oldFunction();
+//  #endif
+//..
+
+/// AUTHNBASIC release major version
+#define AUTHNBASIC_VERSION_MAJOR 99
+
+/// AUTHNBASIC release minor version
+#define AUTHNBASIC_VERSION_MINOR 99
+
+/// AUTHNBASIC patch level
+#define AUTHNBASIC_VERSION_PATCH 99
+
+/// Construct a composite version number in the range [ 0 .. 999900 ] from
+/// the specified 'major' and 'minor' version numbers.  The resulting value,
+/// when expressed as a 6-digit decimal string, has "00" as the two
+/// lowest-order decimal digits, 'minor' as the next two digits, and 'major'
+/// as the highest-order digits.  The result is unique for each combination
+/// of 'major' and 'minor', and is sortable such that a value composed from
+/// a given 'major' version number will compare larger than a value composed
+/// from a smaller 'major' version number (and similarly for 'minor' version
+/// numbers).  Note that if 'major' and 'minor' are both compile-time
+/// integral constants, then the resulting expression is also a compile-time
+/// integral constant.  Also note that the patch version number is
+/// intentionally not included.  The behavior is undefined unless 'major'
+/// and 'minor' are integral values in the range '[ 0 .. 99 ]'.
+#define AUTHNBASIC_MAKE_VERSION(major, minor) ((major) * 10000 + (minor) * 100)
+
+/// Similar to AUTHNBASIC_MAKE_VERSION(), but include patch number as well.
+#define AUTHNBASIC_MAKE_EXT_VERSION(major, minor, patch)                      \
+    ((major) * 10000 + (minor) * 100 + (patch))
+
+/// Construct a composite version number in the range [ 0 .. 999900 ] from
+/// the specified 'AUTHNBASIC_VERSION_MAJOR' and 'AUTHNBASIC_VERSION_MINOR'
+/// numbers corresponding to the major and minor version numbers,
+/// respectively, of the current (latest) AUTHNBASIC release.  Note that the
+/// patch version number is intentionally not included.  For example,
+/// 'AUTHNBASIC_VERSION' produces 10300 (decimal) for AUTHNBASIC version
+/// 1.3.1.
+#define AUTHNBASIC_VERSION                                                    \
+    AUTHNBASIC_MAKE_VERSION(AUTHNBASIC_VERSION_MAJOR, AUTHNBASIC_VERSION_MINOR)
+
+/// Similar to AUTHNBASIC_VERSION, but include the patch number as well
+#define AUTHNBASIC_EXT_VERSION                                                \
+    AUTHNBASIC_MAKE_EXT_VERSION(AUTHNBASIC_VERSION_MAJOR,                     \
+                                AUTHNBASIC_VERSION_MINOR,                     \
+                                AUTHNBASIC_VERSION_PATCH)
+
+#endif
