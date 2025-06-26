d65a577b3be6887f736123ad7660bdb62350dd1c
Dmitrii Petukhov <dpetukhov@bloomberg.net>
2025-06-26 12:15:14 +0300

    Fix merge conflicts
    Signed-off-by: Dmitrii Petukhov <dpetukhov@bloomberg.net>

--- src/python/blazingmq/dev/it/cluster.py
+++ src/python/blazingmq/dev/it/cluster.py
@@ -303,13 +303,7 @@ class Cluster(contextlib.AbstractContextManager):
 
         self.wait_status(wait_leader, wait_ready)
 
-<<<<<<< manual-ci
-    def stop_nodes(
-        self,
-    ):
-=======
     def stop_nodes(self, prevent_leader_bounce=False):
->>>>>>> main
         """Stop the nodes in the cluster.
 
         If 'prevent_leader_bounce' is 'True', prevent leader bounce during
@@ -318,20 +312,13 @@ class Cluster(contextlib.AbstractContextManager):
         NOTE: this method does *not* stop the proxies.
         """
 
-<<<<<<< manual-ci
-        self.last_known_leader = None
-
-        self._logger.info("stopping all nodes")
-
-=======
         self._logger.info("stopping all nodes")
         if prevent_leader_bounce:
             for node in self.nodes():
                 if node is not self.last_known_leader:
-                    node.set_quorum(100)
+                    node.set_quorum(QUORUM_TO_ENSURE_NOT_LEADER)
 
         self.last_known_leader = None
->>>>>>> main
         for node in self.nodes():
             with internal_use(node):
                 node.stop()
