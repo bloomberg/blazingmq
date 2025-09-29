7df11109f27c9f440c211f0ecf9e918d59d8a567
Emelia Lei <wlei29@bloomberg.net>
2025-09-29 10:47:43 -0400

    cancel read might cause a deadlock
    Signed-off-by: Emelia Lei <wlei29@bloomberg.net>

--- src/groups/mqb/mqba/mqba_clientsession.cpp
+++ src/groups/mqb/mqba/mqba_clientsession.cpp
@@ -2924,9 +2924,6 @@ void ClientSession::tearDown(const bsl::shared_ptr<void>& session,
 
     BALL_LOG_INFO << description() << ": tearDown";
 
-    // Cancel the reads on the channel
-    d_channel_sp->cancelRead();
-
     // Enqueue an event to the client dispatcher thread and wait for it to
     // finish; only after this will we have the guarantee that no method will
     // try to use the channel.
