ec376e84874a597faba5266568581272928a5686
Emelia Lei <69409730+emelialei88@users.noreply.github.com>
2025-09-30 17:18:50 -0400

    FIX[MQB]: `cancelRead` might cause a deadlock (#948)
    Signed-off-by: Emelia Lei <wlei29@bloomberg.net>

--- src/groups/mqb/mqba/mqba_adminsession.cpp
+++ src/groups/mqb/mqba/mqba_adminsession.cpp
@@ -387,9 +387,6 @@ void AdminSession::tearDown(BSLA_UNUSED const bsl::shared_ptr<void>& session,
 {
     // executed by the *IO* thread
 
-    // Cancel the reads on the channel
-    d_channel_sp->cancelRead();
-
     // Enqueue an event to the client dispatcher thread and wait for it to
     // finish; only after this will we have the guarantee that no method will
     // try to use the channel.
