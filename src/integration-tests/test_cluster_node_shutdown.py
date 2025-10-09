e63b5148322f06a77080a0be0ff3448865300a7b
Evgeny Malygin <emalygin@bloomberg.net>
2025-10-09 12:28:33 -0400

    IT: introduce BMQITError, fix typo
    Signed-off-by: Evgeny Malygin <emalygin@bloomberg.net>

--- src/integration-tests/test_cluster_node_shutdown.py
+++ src/integration-tests/test_cluster_node_shutdown.py
@@ -19,8 +19,9 @@ recovers and keeps working as expected. For detailed documentation and diagrams
 go to the relevant section in the README.md, in this directory.
 """
 
-from threading import Semaphore
+import queue
 from time import sleep
+from typing import List
 
 import blazingmq.dev.it.testconstants as tc
 from blazingmq.dev.it.fixtures import (  # pylint: disable=unused-import
@@ -29,8 +30,15 @@ from blazingmq.dev.it.fixtures import (  # pylint: disable=unused-import
     multi_node,
 )
 from blazingmq.dev.it.process.client import Client
+from blazingmq.dev.it.util import wait_until
 
 pytestmark = order(6)
+
+
+class BMQITError(RuntimeError):
+    """
+    BMQ IT error.
+    """
 
 
 class TestClusterNodeShutdown:
@@ -38,6 +46,14 @@ class TestClusterNodeShutdown:
     This suite of test cases exercises node shutdown and subsequent failover
     to another cluster node in the presence of all types of queues.
     """
+
+    @staticmethod
+    def open_or_raise(client: Client, uri: str, flags: List[str]):
+        rc = client.open(uri, flags=flags, succeed=True)
+        if rc != Client.e_SUCCESS:
+            raise BMQITError(
+                f"Failed to open a queue: client = {client}, uri = {uri}, rc = {rc}"
+            )
 
     def setup_cluster(self, cluster: Cluster, domain_urls: tc.DomainUrls):
         du = domain_urls
@@ -47,71 +63,32 @@ class TestClusterNodeShutdown:
         self.proxy1 = next(proxies)
 
         self.producer1 = self.proxy1.create_client("producer1")
-        assert (
-            self.producer1.open(du.uri_priority, flags=["write", "ack"], succeed=True)
-            == Client.e_SUCCESS
-        )
-        assert (
-            self.producer1.open(du.uri_fanout, flags=["write", "ack"], succeed=True)
-            == Client.e_SUCCESS
-        )
-        assert (
-            self.producer1.open(tc.URI_BROADCAST, flags=["write", "ack"], succeed=True)
-            == Client.e_SUCCESS
-        )
+        self.open_or_raise(self.producer1, du.uri_priority, ["write", "ack"])
+        self.open_or_raise(self.producer1, du.uri_fanout, ["write", "ack"])
+        self.open_or_raise(self.producer1, tc.URI_BROADCAST, ["write", "ack"])
 
         self.consumer1 = self.proxy1.create_client("consumer1")
-        assert (
-            self.consumer1.open(du.uri_priority, flags=["read"], succeed=True)
-            == Client.e_SUCCESS
-        )
-        assert (
-            self.consumer1.open(du.uri_fanout_foo, flags=["read"], succeed=True)
-            == Client.e_SUCCESS
-        )
-        assert (
-            self.consumer1.open(du.uri_fanout_bar, flags=["read"], succeed=True)
-            == Client.e_SUCCESS
-        )
-        assert (
-            self.consumer1.open(tc.URI_BROADCAST, flags=["read"], succeed=True)
-            == Client.e_SUCCESS
-        )
+        self.open_or_raise(self.consumer1, du.uri_priority, ["read"])
+        self.open_or_raise(self.consumer1, du.uri_fanout_foo, ["read"])
+        self.open_or_raise(self.consumer1, du.uri_fanout_bar, ["read"])
+        self.open_or_raise(self.consumer1, tc.URI_BROADCAST, ["read"])
 
         # 2: Replica proxy
         self.proxy2 = next(proxies)
 
         self.producer2 = self.proxy2.create_client("producer2")
-        assert (
-            self.producer2.open(du.uri_priority, flags=["write", "ack"], succeed=True)
-            == Client.e_SUCCESS
-        )
-        assert (
-            self.producer2.open(du.uri_fanout, flags=["write", "ack"], succeed=True)
-            == Client.e_SUCCESS
-        )
-        assert (
-            self.producer2.open(tc.URI_BROADCAST, flags=["write", "ack"], succeed=True)
-            == Client.e_SUCCESS
-        )
+        self.open_or_raise(self.producer2, du.uri_priority, ["write", "ack"])
+        self.open_or_raise(self.producer2, du.uri_fanout, ["write", "ack"])
+        self.open_or_raise(self.producer2, tc.URI_BROADCAST, ["write", "ack"])
 
         self.consumer2 = self.proxy2.create_client("consumer2")
-        assert (
-            self.consumer2.open(du.uri_priority, flags=["read"], succeed=True)
-            == Client.e_SUCCESS
-        )
-        assert (
-            self.consumer2.open(du.uri_fanout_foo, flags=["read"], succeed=True)
-            == Client.e_SUCCESS
-        )
-        assert (
-            self.consumer2.open(du.uri_fanout_bar, flags=["read"], succeed=True)
-            == Client.e_SUCCESS
-        )
-        assert (
-            self.consumer2.open(tc.URI_BROADCAST, flags=["read"], succeed=True)
-            == Client.e_SUCCESS
-        )
+        self.open_or_raise(self.consumer2, du.uri_priority, ["read"])
+        self.open_or_raise(self.consumer2, du.uri_fanout_foo, ["read"])
+        self.open_or_raise(self.consumer2, du.uri_fanout_bar, ["read"])
+        self.open_or_raise(self.consumer2, tc.URI_BROADCAST, ["read"])
+
+        # 3: Setup message history
+        self.history = []
 
     def test_primary_shutdown_with_proxy(
         self, multi_node: Cluster, domain_urls: tc.DomainUrls
@@ -132,11 +109,11 @@ class TestClusterNodeShutdown:
     def _post_kill_recover_post(self, cluster, active_node, domain_urls: tc.DomainUrls):
         self._verify_all_queues_operational(domain_urls)  # Prior to kill
 
-        recovery = Semaphore(0)
+        recovery = queue.Queue()
 
-        def release_recovery_if_state_restored(line):
+        def release_recovery_if_state_restored(line: str) -> None:
             if ": state restored" in line:
-                recovery.release()
+                recovery.put(line)
 
         for node in cluster.nodes() + cluster.proxies():
             node.add_async_log_hook(release_recovery_if_state_restored)
@@ -147,22 +124,39 @@ class TestClusterNodeShutdown:
         active_node.check_exit_code = False
         active_node.kill()
 
-        assert recovery.acquire(timeout=120)  # pylint: disable=consider-using-with
+        try:
+            _ = recovery.get(timeout=120)
+        except queue.Empty as ex:
+            # No recovery log observed
+            raise BMQITError("State is not restored") from ex
         sleep(5)
 
         self._verify_all_queues_operational(domain_urls)  # After recovery
 
     def _verify_all_queues_operational(self, domain_urls: tc.DomainUrls):
+        self.history = []
         du = domain_urls
         self._verify(du.uri_priority, du.uri_priority, None, True)
         self._verify(du.uri_fanout, du.uri_fanout_foo, du.uri_fanout_bar, True)
         self._verify(tc.URI_BROADCAST, tc.URI_BROADCAST, None, False)
 
     def _verify(self, uri_post, uri_group1, uri_group2, priority):
-        def check_received_one_of(consumer, uri_group, *expected):
+        def check_received_one_of(consumer: Client, uri_group: str, *expected):
+            # wait_push_event() might be triggered by either uri_group1 or uri_group2,
+            # we might still need to wait to get messages
+            wait_until(
+                lambda: len(consumer.list(uri_group, block=True)) >= 1, timeout=5
+            )
             msgs = consumer.list(uri_group, block=True)
-            assert len(msgs) == 1
-            assert msgs[0].payload in expected
+            self.history.append((str(consumer.name), uri_group, msgs))
+            if len(msgs) != 1:
+                raise BMQITError(
+                    f"Expected 1 message, got: {len(msgs)} msgs.\nMessages history:\n{self.history}"
+                )
+            if msgs[0].payload not in expected:
+                raise BMQITError(
+                    f"Unexpected message payload: {msgs[0].payload} (expected: {expected})"
+                )
             consumer.confirm(uri_group, "*", succeed=True)
 
         def check_both_received_one_of(*expected):
@@ -182,13 +176,13 @@ class TestClusterNodeShutdown:
 
         if uri_group2 is not None:
             assert len(self.consumer1.list(uri_group2, block=True)) == 0
-            assert len(self.consumer1.list(uri_group2, block=True)) == 0
+            assert len(self.consumer2.list(uri_group2, block=True)) == 0
 
         # Main part of the test starts here
 
         self.producer1.post(uri_post, payload=["msg1"], succeed=True, wait_ack=True)
 
-        # One has received it but we don't know which one. We will check after
+        # One has received it, but we don't know which one. We will check after
         # we post another one
         if not priority:
             check_both_received_one_of("msg1")
@@ -196,8 +190,8 @@ class TestClusterNodeShutdown:
         self.producer2.post(uri_post, payload=["msg2"], succeed=True, wait_ack=True)
 
         if priority:
-            # By this point both should have received one but we don't know
-            # who got what
+            # By this point both should have received one,
+            # but we don't know who got what
             check_both_received_one_of("msg1", "msg2")
         else:
             check_both_received_one_of("msg2")
