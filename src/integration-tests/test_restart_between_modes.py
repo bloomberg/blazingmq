9daaa78933d67d8006d79ff160ce47f54dcb7bfa
Dmitrii Petukhov <dpetukhov@bloomberg.net>
2025-11-14 21:51:22 +0200

    Extend purge queue/appId test
    Signed-off-by: Dmitrii Petukhov <dpetukhov@bloomberg.net>

--- src/integration-tests/test_restart_between_modes.py
+++ src/integration-tests/test_restart_between_modes.py
@@ -863,7 +863,13 @@ def test_restart_between_legacy_and_fsm_purge_queue_app(
         - "baz" gets 3 messages
         - "quux" gets the fourth message
 
-    4. POST MORE MESSAGES
+    4. Add/Remove more appIds and purge queues/apps
+        Add "corge" appId
+        Post to fanout queue
+        Remove "foo" appId
+        Post more messages to priority and fanout queues
+        purge priority queue
+        purge app "baz"
     5. SWITCH BACK
         Switch back to the corresponding backup mode
     6. VERIFY AGAIN
@@ -924,15 +930,28 @@ def test_restart_between_legacy_and_fsm_purge_queue_app(
     assert re.match(r"msg3", quux_messages[0].payload)
 
     # 4. POST MORE MESSAGES
+    current_app_ids.append("corge")
+    cluster.set_app_ids(current_app_ids, du)
+    post_few_messages(producer, fanout_queue, ["msg4"])
+
+    current_app_ids.remove("foo")
+    cluster.set_app_ids(current_app_ids, du)
+
     for queue in [priority_queue, fanout_queue]:
-        post_few_messages(producer, queue, ["msg4"])
+        post_few_messages(producer, queue, ["msg5"])
+
+    # Purge priority queue
+    cluster.last_known_leader.purge(du.domain_priority, test_queue, succeed=True)
+    # Purge fanout queue app "quux"
+    cluster.last_known_leader.purge(du.domain_fanout, test_queue, "quux", succeed=True)
 
     # 5. SWITCH BACK
     switch_cluster_mode[1](cluster, producer)
 
     # 6. VERIFY AGAIN
-    check_if_queue_has_n_messages(consumer, priority_queue, 1 + 1)
-    check_if_queue_has_n_messages(consumer, fanout_queue + "?id=foo", 3 + 1)
+    check_if_queue_has_n_messages(consumer, priority_queue, 0)
+    check_if_queue_has_n_messages(consumer, fanout_queue + "?id=foo", 0)
     check_if_queue_has_n_messages(consumer, fanout_queue + "?id=bar", 0)
-    check_if_queue_has_n_messages(consumer, fanout_queue + "?id=baz", 3 + 1)
-    check_if_queue_has_n_messages(consumer, fanout_queue + "?id=quux", 1 + 1)
+    check_if_queue_has_n_messages(consumer, fanout_queue + "?id=baz", 3 + 2)
+    check_if_queue_has_n_messages(consumer, fanout_queue + "?id=quux", 0)
+    check_if_queue_has_n_messages(consumer, fanout_queue + "?id=corge", 0 + 2)
