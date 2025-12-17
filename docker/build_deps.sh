203de74b41e9b9ed76eea0588850ccefc6b5e21e
Patrick M. Niedzielski <patrick@pniedzielski.net>
2025-12-17 04:36:24 +0000

    ci: Build C++03 dependencies
    This patch introduces the ability to build our dependencies BDE and
    NTF with a particular C++ standard version, and specific workflows to
    build our dependencies in C++03 and C++17.
    
    Signed-off-by: Patrick M. Niedzielski <pniedzielski@bloomberg.net>

--- docker/build_deps.sh
+++ docker/build_deps.sh
@@ -2,21 +2,53 @@
 
 # This script downloads, builds, and installs the required build dependencies of BMQ
 # from github.com/bloomberg. Software packages are installed to the /opt/bb prefix.
-# If the optional argument '--only-download' is provided, the script will only download
-# dependencies (build and install steps are skipped).
+#
+# Options:
+#   --only-download           Only download dependencies (skip build and install)
+#   --cxx-standard=VERSION    C++ version to use (cpp03, cpp17, etc.). Default: cpp17
 
 set -euxo pipefail
 
-if [ $# == 1 ]; then
-  if [[ $1 == "--only-download" ]]; then
-    DO_BUILD=false
-  else
-     echo "Unexpected optional argument, only '--only-download' is supported"
-     exit 1
-  fi
-else
-    DO_BUILD=true
-fi
+DO_BUILD=true
+CXX_STANDARD=cpp17
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        --only-download)
+            DO_BUILD=false
+            shift
+            ;;
+        --cxx-standard=*)
+            CXX_STANDARD="${1#*=}"
+            shift
+            ;;
+        --cxx-standard)
+            if [ -z "${2:-}" ]; then
+                echo "Error: --cxx-standard requires a value"
+                exit 1
+            fi
+            CXX_STANDARD="$2"
+            shift 2
+            ;;
+        *)
+            echo "Error: Unknown option '$1'"
+            echo "Usage: $0 [--only-download] [--cxx-standard=VERSION]"
+            exit 1
+            ;;
+    esac
+done
+
+# Validate CXX_STANDARD
+case $CXX_STANDARD in
+    cpp03|cpp11|cpp14|cpp17|cpp20|cpp23)
+        # Valid C++ standard version
+        ;;
+    *)
+        echo "Error: Invalid C++ standard version '$CXX_STANDARD'"
+        echo "Supported standard versions: cpp03, cpp11, cpp14, cpp17, cpp20, cpp23"
+        exit 1
+        ;;
+esac
 
 fetch_git() {
     local org=$1
@@ -49,7 +81,7 @@ fetch_deps() {
 configure() {
     PATH="$PATH:$(realpath srcs/bde-tools/bin)"
     export PATH
-    eval "$(bbs_build_env -u opt_64_cpp17)"
+    eval "$(bbs_build_env -u "opt_64_$CXX_STANDARD")"
 }
 
 build_bde() {
@@ -71,7 +103,7 @@ build_ntf() {
         --with-zlib                  \
         --without-zstd               \
         --without-lz4                \
-        --ufid opt_64_cpp17
+        --ufid "opt_64_$CXX_STANDARD"
     make -j8
     make install
     popd
