7479e4b0ea9cc86d1c83014c2aafd12ce2af63f5
Yuan Jing Vincent Yan <yyan82@bloomberg.net>
2026-02-12 13:14:24 -0500

    mqbc::StorageMgr: Determine data destinations before push or drop
    Signed-off-by: Yuan Jing Vincent Yan <yyan82@bloomberg.net>

--- src/groups/mqb/mqbc/mqbc_recoverymanager.cpp
+++ src/groups/mqb/mqbc/mqbc_recoverymanager.cpp
@@ -15,6 +15,7 @@
 
 #include <ball_log.h>
 #include <bsls_assert.h>
+#include <mqbc_partitionfsm.h>
 #include <mqbc_recoverymanager.h>
 #include <mqbs_qlistfileiterator.h>
 
@@ -434,8 +435,9 @@ void RecoveryManager::setExpectedDataChunkRange(
     ReceiveDataContext& receiveDataCtx = recoveryCtx.d_receiveDataContext;
     if (receiveDataCtx.d_expectChunks) {
         BALL_LOG_ERROR << d_clusterData.identity().description()
-                       << " Partition [" << partitionId << "]: "
-                       << "Got notification to expect chunks when self is "
+                       << " Partition [" << partitionId
+                       << "]: " << PartitionFSM::k_PFSM_DEFECT_LOG_TAG
+                       << " Got notification to expect chunks when self is "
                        << "already expecting chunks.  Self's view: "
                        << "recovery requestId = "
                        << receiveDataCtx.d_recoveryRequestId
