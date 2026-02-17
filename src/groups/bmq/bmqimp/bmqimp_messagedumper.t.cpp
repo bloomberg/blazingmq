a924a9cd5ea1c7a63a60c2c10db6f1e9891437c8
Patrick M. Niedzielski <pniedzielski@bloomberg.net>
2026-02-17 16:38:09 -0500

    Fix[bmqimp]: Let `MessageDumper` test handle authentication
    Clang correctly warns that the `Tester` helper class in
    `bmqimp_messagedumper.t.cpp` does not handle authentication events:
    
        Warning: blazingmq/src/groups/bmq/bmqimp/bmqimp_messagedumper.t.cpp:590:13: warning: enumeration value 'e_AUTHENTICATION' not explicitly handled in switch [-Wswitch-enum]
            switch (type) {
                    ^~~~
        blazingmq/src/groups/bmq/bmqimp/bmqimp_messagedumper.t.cpp:590:13: note: add missing switch cases
            switch (type) {
                    ^
    
    This patch adds a case for these events.
    
    Signed-off-by: Patrick M. Niedzielski <pniedzielski@bloomberg.net>

--- src/groups/bmq/bmqimp/bmqimp_messagedumper.t.cpp
+++ src/groups/bmq/bmqimp/bmqimp_messagedumper.t.cpp
@@ -607,6 +607,7 @@ bool Tester::isEventDumpEnabled(const bmqp::EventType::Enum& type) const
         CASE(bmqp::EventType::e_HEARTBEAT_REQ)
         CASE(bmqp::EventType::e_HEARTBEAT_RSP)
         CASE(bmqp::EventType::e_REPLICATION_RECEIPT)
+        CASE(bmqp::EventType::e_AUTHENTICATION)
         CASE(bmqp::EventType::e_UNDEFINED)
     default: {
         result =
