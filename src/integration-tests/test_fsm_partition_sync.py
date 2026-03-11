a4a56beb1a103f2bf9c4208c93d0c1f5546d3c1a
Aleksandr Ivanov <aivanov71@bloomberg.net>
2026-03-11 16:25:33 +0200

    Remove time stamps from storage tool summary in IT
    Signed-off-by: Aleksandr Ivanov <aivanov71@bloomberg.net>

--- src/integration-tests/test_fsm_partition_sync.py
+++ src/integration-tests/test_fsm_partition_sync.py
@@ -76,15 +76,35 @@ def _clean_output(output_str: str) -> str:
     """
     Clean the output by removing any non-deterministic parts, such as timestamps/epochs.
     """
-    KEYS_TO_REMOVE = [
+    RECORDS_KEYS_TO_REMOVE = [
         "Timestamp",
         "Epoch",
     ]
+    SUMMARY_KEYS_TO_REMOVE = [
+        "First SyncPointRecord timestamp",
+        "First SyncPointRecord epoch",
+        "Record Timestamp",
+        "Record Epoch",
+        "SyncPoint Timestamp",
+        "SyncPoint Epoch"
+    ]
     data = json.loads(output_str)
-    for record in data["Records"]:
-        for key in KEYS_TO_REMOVE:
-            if key in record:
-                del record[key]
+    if "Records" in data:
+        for record in data["Records"]:
+            for key in RECORDS_KEYS_TO_REMOVE:
+                if key in record:
+                    del record[key]
+    elif "JournalFileDetails" in data:
+        journal_details = data["JournalFileDetails"]
+        journal_hdr = journal_details["Journal File Header"]
+        for key in SUMMARY_KEYS_TO_REMOVE:
+            if key in journal_hdr:
+                del journal_hdr[key]
+        if "Journal SyncPoint" in journal_details:
+            journal_sync = journal_details["Journal SyncPoint"]
+            for key in SUMMARY_KEYS_TO_REMOVE:
+                if key in journal_sync:
+                    del journal_sync[key]
 
     return json.dumps(data)
 
@@ -151,7 +171,7 @@ def _stop_cluster_and_compare_journal_files(
         )
 
         # Check that content of leader and replica journal files is equal
-        assert leader_res.stdout == replica_res.stdout, (
+        assert _clean_output(leader_res.stdout) == _clean_output(replica_res.stdout), (
             f"Leader and replica journal file summary differ for {leader_file} and {replica_file}"
         )
 
