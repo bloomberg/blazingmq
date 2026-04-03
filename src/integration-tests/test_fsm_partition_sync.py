a06e675b3528fbed5ec634bc4e18f01b053eccdf
Aleksandr Ivanov <aivanov71@bloomberg.net>
2026-04-03 18:10:59 +0300

    Fix code formatting
    Signed-off-by: Aleksandr Ivanov <aivanov71@bloomberg.net>

--- src/integration-tests/test_fsm_partition_sync.py
+++ src/integration-tests/test_fsm_partition_sync.py
@@ -540,7 +540,7 @@ def test_sync_if_leader_missed_rollover(
     for node in cluster.nodes(exclude=[leader, next_leader]):
         assert node.outputs_substr("ROLLOVER COMPLETE", 3), (
             f"Node {node.name} did not complete rollover"
-        )        
+        )
 
     # Stop all running nodes
     for node in cluster.nodes(exclude=next_leader):
