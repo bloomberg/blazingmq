c6ce09fc53dfeb484b80dd89d25fddd9a903e777
28arnab <68094959+28arnab@users.noreply.github.com>
2026-04-08 20:21:32 +0530

    Fix removed:unused variables in bmqa_abstractsession.t.cpp #1271

--- src/groups/bmq/bmqa/bmqa_abstractsession.t.cpp
+++ src/groups/bmq/bmqa/bmqa_abstractsession.t.cpp
@@ -328,11 +328,6 @@ static void test1_breathingTest()
     bmqt::Uri                       dummyUri;
     bsls::TimeInterval              dummyTimeInterval(0);
 
-    bmqa::OpenQueueStatus openQueueResult(bmqtst::TestHelperUtil::allocator());
-    bmqa::ConfigureQueueStatus configureQueueResult(
-        bmqtst::TestHelperUtil::allocator());
-    bmqa::CloseQueueStatus closeQueueResult(
-        bmqtst::TestHelperUtil::allocator());
 
     const bmqa::AbstractSession::OpenQueueCallback      openQueueCallback;
     const bmqa::AbstractSession::ConfigureQueueCallback configureQueueCallback;
@@ -470,11 +465,6 @@ static void test2_instanceInvariants()
     bmqt::Uri dummyUri(bmqtst::TestHelperUtil::allocator());
     bsls::TimeInterval              dummyTimeInterval(0);
 
-    bmqa::OpenQueueStatus openQueueResult(bmqtst::TestHelperUtil::allocator());
-    bmqa::ConfigureQueueStatus configureQueueResult(
-        bmqtst::TestHelperUtil::allocator());
-    bmqa::CloseQueueStatus closeQueueResult(
-        bmqtst::TestHelperUtil::allocator());
 
     const bmqa::AbstractSession::OpenQueueCallback      openQueueCallback;
     const bmqa::AbstractSession::ConfigureQueueCallback configureQueueCallback;
