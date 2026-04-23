5b62254840a8c81eeca4024d5466b54fd9b8e9fd
Evgeny Malygin <emalygin@bloomberg.net>
2026-04-23 14:46:44 -0400

    Feat[mqb]: store AppConfig value, not a pointer to config
    Signed-off-by: Evgeny Malygin <emalygin@bloomberg.net>

--- src/groups/mqb/mqbcfg/mqbcfg_brokerconfig.cpp
+++ src/groups/mqb/mqbcfg/mqbcfg_brokerconfig.cpp
@@ -16,8 +16,16 @@
 // mqbcfg_brokerconfig.cpp                                            -*-C++-*-
 #include <mqbcfg_brokerconfig.h>
 
+// MQB
+#include <mqbcfg_messages.h>
+
+// BMQ
+#include <bmqu_singletonallocator.h>
+
 // BDE
+#include <bslmt_once.h>
 #include <bsls_assert.h>
+#include <bsls_objectbuffer.h>
 
 namespace BloombergLP {
 namespace mqbcfg {
@@ -25,6 +33,18 @@ namespace mqbcfg {
 namespace {
 
 const AppConfig* s_config_p;
+
+mqbcfg::AppConfig* getImpl()
+{
+    static mqbcfg::AppConfig* s_appConfig_p;
+    BSLMT_ONCE_DO
+    {
+        bslma::Allocator* alloc = bmqu::SingletonAllocator::allocator();
+        s_appConfig_p = bslma::AllocatorUtil::newObject<mqbcfg::AppConfig>(
+            alloc);
+    }
+    return s_appConfig_p;
+}
 
 }  // close unnamed namespace
 
@@ -35,14 +55,15 @@ const AppConfig* s_config_p;
 // MANIPULATORS
 void BrokerConfig::set(const AppConfig& config)
 {
-    BSLS_ASSERT_SAFE(!s_config_p && "config already set");
-    s_config_p = &config;
+    BSLS_ASSERT_OPT(!s_config_p && "config already set");
+    *getImpl() = config;
+    s_config_p = getImpl();
 }
 
 // ACCESSORS
 const AppConfig& BrokerConfig::get()
 {
-    BSLS_ASSERT_SAFE(s_config_p && "config not set");
+    BSLS_ASSERT_OPT(s_config_p && "config not set");
     return *s_config_p;
 }
 
