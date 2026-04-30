c8a7a67a6a04b6dbefd1ca29c8d55e66bcd294e8
Patrick M. Niedzielski <pniedzielski@bloomberg.net>
2026-04-30 16:38:52 -0400

    Refactor: Remove bmqbrkrscm versioning
    Broker versions move in lockstep with libmqb versions, so it does not
    make sense for us to have separate version information for the two.
    The broker version is only used directly when printing a response to
    `--version` and to pass it down further into libmqb, so simply need to
    replace those two uses with the corresponding libmqb versions.
    
    This patch removes the `bmqbrkrscm_version` and
    `bmqbrkrscm_versiontag` components, and replaces all their uses with
    `mqbscm_version` and `mqbscm_versiontag`.  Additionally, we clean up
    the output of `--version` to print the version string directly, rather
    than an identifier that contains the version string.
    
    Signed-off-by: Patrick M. Niedzielski <pniedzielski@bloomberg.net>

--- src/applications/bmqbrkr/bmqbrkr.m.cpp
+++ src/applications/bmqbrkr/bmqbrkr.m.cpp
@@ -14,13 +14,13 @@
 // limitations under the License.
 
 // Application
-#include <bmqbrkrscm_version.h>
 #include <m_bmqbrkr_task.h>
 
 // MQB
 #include <mqba_application.h>
 #include <mqbcfg_brokerconfig.h>
 #include <mqbcfg_messages.h>
+#include <mqbscm_version.h>
 #include <mqbu_exit.h>
 #include <mqbu_messageguidutil.h>
 
@@ -291,7 +291,7 @@ static int getConfig(bsl::ostream&          errorDescription,
     }
 
     config->appConfig().etcDir()        = configDir;
-    config->appConfig().brokerVersion() = bmqbrkrscm::Version::versionAsInt();
+    config->appConfig().brokerVersion() = mqbscm::Version::versionAsInt();
 
     return rc_SUCCESS;
 }
@@ -641,7 +641,7 @@ int main(int argc, const char* argv[])
 
         if (version) {
             bsl::cout << "BlazingMQ broker version: "
-                      << bmqbrkrscm::Version::version() << "\n";
+                      << mqbscm::Version::s_versionDotString << "\n";
             return 0;
         }
 
