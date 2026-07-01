024e8d958f62cd3f06cddc0792586466887869e1
Jake Jendzurski <jakeski2015@gmail.com>
2026-07-01 17:03:41 -0400

    first draft of fuzzer for Put Message Iterator
    Signed-off-by: Jake Jendzurski <jakeski2015@gmail.com>

---
+++ src/standalones/s_bmqfuzz/s_bmqfuzz_bmqp_putmessageiterator.fuzz.cpp
@@ -0,0 +1,52 @@
+// Copyright 2025 Bloomberg Finance L.P.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <bmqp_putmessageiterator.h>
+
+#include <bdlbb_blobutil.h>
+#include <bdlbb_pooledblobbufferfactory.h>
+#include <bsl_cstring.h>
+
+using namespace BloombergLP;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    if (size < sizeof(bmqp::EventHeader)) {
+        return 0;
+    }
+
+    bslma::Allocator* alloc = bslma::Default::defaultAllocator();
+    bdlbb::PooledBlobBufferFactory bufferFactory(1024, alloc);
+
+    bdlbb::Blob blob(&bufferFactory, alloc);
+    bdlbb::BlobUtil::append(&blob,
+                            reinterpret_cast<const char*>(data),
+                            static_cast<int>(size));
+
+    bmqp::EventHeader eventHeader;
+    bsl::memcpy(&eventHeader, data, sizeof(bmqp::EventHeader));
+
+    bmqp::PutMessageIterator iter(&bufferFactory, alloc);
+    int rc = iter.reset(&blob, eventHeader, false);
+    if (rc != 0) {
+        return 0;
+    }
+
+    while (iter.next() == 1) {
+        iter.header();
+    }
+
+    return 0;
+}
