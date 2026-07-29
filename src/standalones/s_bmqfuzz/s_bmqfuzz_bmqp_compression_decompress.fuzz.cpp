475145b13b7032cae90e2521477c353f1972627e
Patrick M. Niedzielski <pniedzielski@bloomberg.net>
2026-07-29 17:04:06 +0000

    Fix: Add maxOutputSize to decompress fuzz test (#1661)
    Signed-off-by: Patrick M. Niedzielski <pniedzielski@bloomberg.net>

--- src/standalones/s_bmqfuzz/s_bmqfuzz_bmqp_compression_decompress.fuzz.cpp
+++ src/standalones/s_bmqfuzz/s_bmqfuzz_bmqp_compression_decompress.fuzz.cpp
@@ -45,6 +45,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
                                   &bufferFactory,
                                   bmqt::CompressionAlgorithmType::e_ZLIB,
                                   input,
+                                  64 * 1024 * 1024,  // maxOutputSize cap
                                   &errorStream,
                                   alloc);
 
