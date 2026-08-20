b2f575c05e277078e2dce725b661934ffd256571
Taylor Foxhall <tfoxhall@bloomberg.net>
2026-08-20 20:09:50 +0000

    Feat: Add simple policy description VSTs
    Signed-off-by: Taylor Foxhall <tfoxhall@bloomberg.net>

--- bin/codegen.sh
+++ bin/codegen.sh
@@ -93,6 +93,9 @@ script with the CODEGEN enviornment variable set to the internal codegen tool." 
     mqbact)
       codegen ./src/groups/mqb/mqbact mqbact.xsd mqbact actions
       ;;
+    mqbpoly)
+      codegen ./src/groups/mqb/mqbpoly mqbpoly.xsd mqbpoly policies
+      ;;
     *)
       usage
       exit 1
